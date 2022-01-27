844cff5f24803c309076c1850842ec9bd83596f4
Clément Prod'homme <contact@clementprodhomme.fr>
2022-01-27 14:27:14 +0100

    Add the platform's font sizes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
       sans: ['Work\\ Sans', 'sans-serif'],
       serif: ['Cormorant', 'serif'],
     },
+    fontSize: {
+      '2xs': ['.625rem', '.75rem'],
+      xs: ['.75rem', '1.125rem'],
+      sm: ['.875rem', '1.25rem'],
+      base: ['1rem', '1.5rem'],
+      lg: ['1.125rem', '1.75rem'],
+      xl: ['1.3rem', '2rem'],
+      '2xl': ['2rem', '2.5rem'],
+      '3xl': ['2.5rem', '3rem'],
+      '4xl': ['3.5rem', '4rem'],
+      '5xl': ['4rem', '5rem'],
+      '6xl': ['5.5rem', '6.5rem'],
+    },
     colors: {
       background: {
         light: '#FFFDFA',
