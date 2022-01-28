b32fabd2a904366f40e7dd3c6fca7e5ae1615d90
Tomasz Subik <tsubik@gmail.com>
2022-01-28 13:58:22 +0100

    backend scaffold

---
+++ backend/spec/rails_helper.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+
+require 'test_prof/recipes/rspec/before_all'
+require 'test_prof/recipes/rspec/let_it_be'
+
+require 'super_diff/rspec-rails'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.request_snapshots_dir = 'spec/fixtures/snapshots'
+
+  config.include FactoryBot::Syntax::Methods
+  config.include Rails.application.routes.url_helpers, type: :request
+
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
