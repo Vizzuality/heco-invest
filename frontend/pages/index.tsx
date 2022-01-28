870aba2757829260fbd76c51b399ac4aca42f9b8
Clément Prod'homme <contact@clementprodhomme.fr>
2022-01-28 16:50:22 +0100

    Display the site map on the homepage
    This is temporary to track the progress of the implementation of the static pages.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,42 @@
 import Head from 'next/head';
 
-const Home: React.FC = () => (
+import LayoutContainer from 'components/layout-container';
+import { StaticPageLayoutProps } from 'layouts/static-page';
+import { PageComponent } from 'types';
+
+const Home: PageComponent<{}, StaticPageLayoutProps> = () => (
   <>
     <Head>
       <title>HeCo Invest</title>
     </Head>
-    <h1>Welcome to HeCo Invest!</h1>
+    <LayoutContainer>
+      <h1>Welcome to HeCo Invest!</h1>
+      <p>Site map:</p>
+      <ol>
+        <li>/</li>
+        <li>/investors</li>
+        <li>/project-developers</li>
+        <li>/about</li>
+        <li>/faq</li>
+        <li>/privacy-policy</li>
+        <li>/discover/:tab?</li>
+        <li>/project/:id</li>
+        <li>/project-developer/:id</li>
+        <li>/open-call/:id</li>
+        <li>/investor/:id</li>
+        <li>/500</li>
+        <li>/404</li>
+      </ol>
+    </LayoutContainer>
   </>
 );
+
+Home.layout = {
+  props: {
+    mainProps: {
+      className: 'mb-7 lg:mb-24',
+    },
+  },
+};
 
 export default Home;
