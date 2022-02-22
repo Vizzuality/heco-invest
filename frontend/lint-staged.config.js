8fa8b272a49b87dd14c59eb8afb6b1cea46b028e
Tomasz Subik <tsubik@gmail.com>
2022-02-22 19:11:00 +0100

    switch to lefthook

--- frontend/lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-// lint-staged.config.js
-// workaround for type checks https://github.com/microsoft/TypeScript/issues/27379#issuecomment-883154194
-module.exports = {
-  '*.{js,jsx,ts,tsx}': 'eslint --fix',
-  '**/*.ts?(x)': () => 'tsc --noEmit',
-};
