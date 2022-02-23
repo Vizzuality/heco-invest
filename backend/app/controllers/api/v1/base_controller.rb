49bd1f0ff673e1813f454910fa60614528d38a3a
Tomasz Subik <tsubik@gmail.com>
2022-02-23 11:13:01 +0100

    setup api base controller

---
+++ backend/app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,24 @@
+module API
+  module V1
+    class BaseController < ActionController::API
+      include ActionController::Cookies
+      include ActionController::RequestForgeryProtection
+      include API::Errors
+      include API::Authentication
+
+      protect_from_forgery with: :exception
+
+      wrap_parameters format: [:json]
+
+      before_action :require_json!
+
+      private
+
+      def require_json!
+        return if request.get? || request.content_type.to_s.starts_with?("application/json")
+
+        raise API::Error, "application/json content type is required by the requested endpoint"
+      end
+    end
+  end
+end
