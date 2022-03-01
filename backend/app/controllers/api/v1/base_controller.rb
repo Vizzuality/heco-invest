1a90bbe71bfe4543f0245a84a92af7c9aa0dc9c2
Tomasz Subik <tsubik@gmail.com>
2022-03-01 18:43:54 +0100

    Add locale to API

--- backend/app/controllers/api/v1/base_controller.rb
+++ backend/app/controllers/api/v1/base_controller.rb
@@ -11,6 +11,7 @@ module API
       wrap_parameters format: [:json]
 
       before_action :require_json!
+      around_action :set_locale
 
       private
 
@@ -18,6 +19,15 @@ module API
         return if request.get? || request.content_type.to_s.starts_with?("application/json")
 
         raise API::Error, "application/json content type is required by the requested endpoint"
+      end
+
+      def set_locale(&action)
+        locale = if params[:locale].present? && I18n.available_locales.map(&:to_s).include?(params[:locale])
+          params[:locale]
+        else
+          I18n.default_locale.to_s
+        end
+        I18n.with_locale(locale, &action)
       end
     end
   end
--- backend/app/serializers/api/v1/investor_serializer.rb
+++ backend/app/serializers/api/v1/investor_serializer.rb
@@ -4,7 +4,7 @@ module API
       include JSONAPI::Serializer
 
       attributes :name, :slug, :about, :website, :instagram, :facebook, :linkedin, :twitter,
-        :how_do_you_work, :what_makes_the_difference, :other_information,
+        :how_do_you_work, :what_makes_the_difference, :other_information, :investor_type,
         :categories, :ticket_sizes, :instrument_types, :impacts, :sdgs,
         :previously_invested, :previously_invested_description
     end
