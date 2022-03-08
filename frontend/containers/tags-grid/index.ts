58c14bae3b20ab17c8838bd9d796143445202fc8
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-03-08 16:48:42 +0000

    Add TagsGrid component

---
+++ frontend/containers/tags-grid/index.ts
@@ -0,0 +1,2 @@
+export { default } from './component';
+export type { TagsGridProps, TagsGridRowType } from './types';
---
+++ frontend/containers/tags-grid/types.ts
@@ -0,0 +1,17 @@
+import type { CategoryType } from 'containers/category-tag';
+
+export type TagsGridRowType = {
+  /** Label for the tags list */
+  title: string;
+  /** Type of tags to display. Defaults to `default` */
+  type?: 'default' | 'category';
+  /** Array of strings for `default` tags, or array of { id, title } for `category` tags */
+  tags: string[] | { id: CategoryType; title: string }[];
+};
+
+export type TagsGridProps = {
+  /** Classnames to apply to the element's wrapper */
+  className?: string;
+  /** Array of rows for the grid */
+  rows: TagsGridRowType[];
+};
