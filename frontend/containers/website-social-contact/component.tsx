ece2f304e0cd9d86dcdcec143095f4e0d510e6a9
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-03-09 14:32:48 +0000

    Reorganize WebsiteSocialContact component fields

--- frontend/containers/website-social-contact/component.tsx
+++ frontend/containers/website-social-contact/component.tsx
@@ -18,75 +18,78 @@ export const WebsiteSocialContact: FC<WebsiteSocialContactProps> = ({
   contact,
 }: WebsiteSocialContactProps) => {
   return (
-    <div className={className}>
-      <div className="grid gap-3 md:max-w-sm break-word grid-cols-1fr-auto md:grid-cols-auto-1fr">
-        {website && (
-          <>
-            <span className="text-gray-800">
-              <FormattedMessage defaultMessage="Website" id="JkLHGw" />
-            </span>
-            <span>
-              <Link href={website}>
-                <a
-                  className="transition-all rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-dark"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {website}
-                </a>
-              </Link>
-            </span>
-          </>
-        )}
+    <div
+      className={cx({
+        'grid gap-y-3 gap-x-12 break-word grid-cols-1fr-auto md:grid-cols-auto-1fr': true,
+        [className]: !!className,
+      })}
+    >
+      {contact && (
+        <>
+          <span className="text-gray-800">
+            <FormattedMessage defaultMessage="Person of contact" id="3l2y4U" />
+          </span>
+          <span>{contact}</span>
+        </>
+      )}
 
-        {social.length > 0 && (
-          <>
-            <span className="text-gray-800 ">
-              <FormattedMessage defaultMessage="Social media" id="ZEEVQX" />
-            </span>
-            <span className="flex items-center gap-2">
-              {social.map(({ id, url }: SocialType) => {
-                const socialItem = SOCIAL_DATA.find((sd) => sd.id === id);
-                const { icon, title } = socialItem;
-                if (!socialItem) return null;
-                return (
-                  <Link key={id} href={url}>
-                    <a
-                      className="transition-all rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-dark"
-                      target="_blank"
-                      rel="noopener noreferrer"
+      {website && (
+        <>
+          <span className="text-gray-800">
+            <FormattedMessage defaultMessage="Website" id="JkLHGw" />
+          </span>
+          <span>
+            <Link href={website}>
+              <a
+                className="transition-all rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-dark"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {website}
+              </a>
+            </Link>
+          </span>
+        </>
+      )}
+
+      {social.length > 0 && (
+        <>
+          <span className="text-gray-800 ">
+            <FormattedMessage defaultMessage="Social media" id="ZEEVQX" />
+          </span>
+          <span className="flex items-center gap-2">
+            {social.map(({ id, url }: SocialType) => {
+              const socialItem = SOCIAL_DATA.find((sd) => sd.id === id);
+              const { icon, title } = socialItem;
+              if (!socialItem) return null;
+              return (
+                <Link key={id} href={url}>
+                  <a
+                    className="transition-all rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-dark"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span
+                      key={id}
+                      className="block w-5 h-5 px-0.5 bg-black rounded"
+                      aria-label={title}
                     >
-                      <span
-                        key={id}
-                        className="block w-5 h-5 px-0.5 bg-black rounded"
-                        aria-label={title}
-                      >
-                        <Icon
-                          aria-hidden={true}
-                          icon={icon}
-                          className={cx({
-                            'w-full h-full fill-white': true,
-                            'stroke-0': id !== 'instagram',
-                          })}
-                        />
-                      </span>
-                    </a>
-                  </Link>
-                );
-              })}
-            </span>
-          </>
-        )}
-
-        {contact && (
-          <>
-            <span className="text-gray-800">
-              <FormattedMessage defaultMessage="Person of contact" id="3l2y4U" />
-            </span>
-            <span>{contact}</span>
-          </>
-        )}
-      </div>
+                      <Icon
+                        aria-hidden={true}
+                        icon={icon}
+                        className={cx({
+                          'w-full h-full fill-white': true,
+                          'stroke-0': id !== 'instagram',
+                        })}
+                      />
+                    </span>
+                  </a>
+                </Link>
+              );
+            })}
+          </span>
+        </>
+      )}
     </div>
   );
 };
