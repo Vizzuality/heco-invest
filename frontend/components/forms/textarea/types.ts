ddf939fe9a81af5692435d95e4fa9b7cfc7d33f4
Clément Prod'homme <clementprdhomme@users.noreply.github.com>
2022-03-21 10:08:13 +0100

    Implement the textarea component (#31)

---
+++ frontend/components/forms/textarea/types.ts
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Path, UseFormRegister, RegisterOptions, FieldPath } from 'react-hook-form';
+
+export enum TextareaResize {
+  Vertically = 'resize-y',
+  Horizontally = 'resize-x',
+  Resize = 'resize',
+  None = 'resize-none',
+}
+
+export type TextareaProps<FormValues> = {
+  /** ID of the element. Required if `aria-label` is not provided. */
+  id?: string;
+  /** Label of the element. Required `id` is not provided. */
+  'aria-label'?: string;
+  /** Number of rows to display. Defaults to `3` */
+  rows?: number;
+  /** Whether (and how) to allow the textarea to be resized. Defaults to `None` */
+  resize?: TextareaResize;
+  /** Name of the textarea */
+  name: Path<FormValues>;
+  /** React Hook Form's `register` function */
+  register: UseFormRegister<FormValues>;
+  /** Options for React Hook Form's `register` function */
+  registerOptions?: RegisterOptions<FormValues, FieldPath<FormValues>>;
+} & Omit<
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+  keyof RegisterOptions<FormValues, FieldPath<FormValues>>
+>;
