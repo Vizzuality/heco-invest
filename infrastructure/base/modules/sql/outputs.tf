74e8d7e67c2a47245f7b2198f56fedffc8921eee
Tiago Garcia <tiagojsag@gmail.com>
2022-03-29 12:29:45 +0200

    Fix issues with stging+production deployment

--- infrastructure/base/modules/sql/outputs.tf
+++ infrastructure/base/modules/sql/outputs.tf
@@ -1,3 +1,7 @@
 output "database_host" {
   value = google_sql_database_instance.db-main.private_ip_address
 }
+
+output "database" {
+  value = google_sql_database.database
+}
--- infrastructure/base/outputs.tf
+++ infrastructure/base/outputs.tf
@@ -2,10 +2,22 @@ output "staging_dns_name_servers" {
   value = module.staging.dns_name_servers
 }
 
-output "production_site_url" {
+output "staging_site_url" {
   value = module.staging.site_url
 }
 
-output "production_api_url" {
+output "staging_api_url" {
   value = module.staging.api_url
+}
+
+output "production_dns_name_servers" {
+  value = module.production.dns_name_servers
+}
+
+output "production_site_url" {
+  value = module.production.site_url
+}
+
+output "production_api_url" {
+  value = module.production.api_url
 }
