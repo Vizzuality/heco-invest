91c22576c8528bcbdacf032cfd1e55e1599a4e32
barbara-chaves <barbarabarcellos90@gmail.com>
2022-04-13 12:41:23 +0200

    feat: adjust header to sign-in Change menu sections and items with diferent screen sizes Change menu if the user is signed in

--- frontend/layouts/static-page/header/component.tsx
+++ frontend/layouts/static-page/header/component.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 
 import { ChevronDown, Menu as MenuIcon } from 'react-feather';
 import { FormattedMessage, useIntl } from 'react-intl';
