19138d8964c0e199b00aee67d2e29cede46025cd
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2022-04-13 13:52:50 +0200

    Contact email format validation

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -23,6 +23,7 @@ class Account < ApplicationRecord
   validates :language, inclusion: {in: Language::TYPES}
   validates :picture, attached: true, content_type: /\Aimage\/.*\z/
   validates :contact_email, presence: true
+  validates :contact_email, format: {with: Devise.email_regexp}, unless: -> { contact_email.blank? }
 
   def slug_preview
     set_slug unless slug.present?
