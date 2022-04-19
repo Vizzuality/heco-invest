f6e0a1ad0511408e54344487edd8828134fc293a
barbara-chaves <barbarabarcellos90@gmail.com>
2022-04-19 14:10:29 +0200

    feat: adjust page header delete getUserInitials file

--- frontend/helpers/getUserInitials.ts
+++
@@ -1,9 +0,0 @@
-import { User } from 'types/user';
-
-export default (user: User) => {
-  if (user?.attributes) {
-    const { first_name, last_name } = user.attributes;
-    return `${first_name.substring(0, 1).toUpperCase()}${last_name.substring(0, 1).toUpperCase()}`;
-  }
-  return '';
-};
