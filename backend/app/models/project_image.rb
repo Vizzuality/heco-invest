13e6ef338dce6ce8efe858fcfa55475aa9464d47
martintomas <martintomas.it@gmail.com>
2022-04-21 16:15:26 +0200

    feat: have max one cover per project all the time

--- backend/app/models/project_image.rb
+++ backend/app/models/project_image.rb
@@ -5,4 +5,14 @@ class ProjectImage < ApplicationRecord
 
   validates :file, attached: true, content_type: /\Aimage\/.*\z/, size: {less_than: 5.megabytes}
   validates :cover, inclusion: {in: [true, false]}
+
+  after_save :keep_only_one_cover
+
+  private
+
+  def keep_only_one_cover
+    return unless cover
+
+    ProjectImage.where(project_id: project_id, cover: true).where.not(id: id).update_all cover: false
+  end
 end
--- backend/spec/models/project_image_spec.rb
+++ backend/spec/models/project_image_spec.rb
@@ -24,4 +24,33 @@ RSpec.describe ProjectImage, type: :model do
     subject.cover = nil
     expect(subject).to have(1).errors_on(:cover)
   end
+
+  describe "#keep_only_one_cover" do
+    let!(:old_project_image) { create :project_image, cover: true }
+    let!(:different_project_image) { create :project_image, cover: true }
+
+    context "when cover of new record is false" do
+      before do
+        create :project_image, project: old_project_image.project, cover: false
+      end
+
+      it "does not change cover value of old records" do
+        expect(old_project_image.reload.cover).to be_truthy
+      end
+    end
+
+    context "when cover of new record is true" do
+      before do
+        create :project_image, project: old_project_image.project, cover: true
+      end
+
+      it "sets cover value of previous record to false" do
+        expect(old_project_image.reload.cover).to be_falsey
+      end
+
+      it "does not touch records of different projects" do
+        expect(different_project_image.reload.cover).to be_truthy
+      end
+    end
+  end
 end
