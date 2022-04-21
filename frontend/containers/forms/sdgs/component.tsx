c176eaa00b4423cd0784d1826a0065d2ae8093e2
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-04-21 13:21:05 +0100

    Add form/SDGs component

---
+++ frontend/containers/forms/sdgs/component.tsx
@@ -0,0 +1,70 @@
+import { useMemo } from 'react';
+
+import { FieldValues } from 'react-hook-form';
+import { FormattedMessage } from 'react-intl';
+
+import cx from 'classnames';
+
+import sdgs from 'mockups/sdgs.json';
+
+import SDG from './sdg';
+import { SDGsProps } from './types';
+
+export const SDGs = <FormValues extends FieldValues>({
+  className,
+  name,
+  errors,
+  register,
+  registerOptions,
+  setValue,
+  clearErrors,
+  setValueOptions = { shouldDirty: true },
+}: SDGsProps<FormValues>) => {
+  // Casting as any as a workaround for the following issue:
+  // https://github.com/react-hook-form/react-hook-form/discussions/7246
+  const allValues = useMemo(() => sdgs.map(({ id }) => id) as any, []);
+
+  const handleSelectAllClick = () => {
+    // Set the values on react-hook-form
+    setValue(name, allValues, setValueOptions);
+    // Setting `shouldValidate` in `setValueOptions` will trigger a whole form validation
+    // which we don't want. Since we're simply selecting all values, we can clear
+    // the errors manually
+    clearErrors(name);
+  };
+
+  return (
+    <div
+      className={cx({
+        [className]: !!className,
+      })}
+    >
+      <div role="group">
+        <div className="flex flex-wrap items-center gap-2">
+          {sdgs.map(({ id, attributes }) => (
+            <SDG
+              key={id}
+              id={id}
+              name={name}
+              image={attributes.image}
+              title={attributes.name}
+              register={register}
+              registerOptions={registerOptions}
+              invalid={errors && errors[name]}
+            />
+          ))}
+        </div>
+      </div>
+
+      <button
+        className="mt-6 text-sm font-light underline cursor-pointer text-green-dark"
+        type="button"
+        onClick={handleSelectAllClick}
+      >
+        <FormattedMessage defaultMessage="Select all" id="94Fg25" />
+      </button>
+    </div>
+  );
+};
+
+export default SDGs;
