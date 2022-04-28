ec8d10631876c5ffd5524047615176cf3c184826
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-04-28 10:38:15 +0100

    Add barebones project page and projectService

---
+++ frontend/pages/project/[id]/index.tsx
@@ -0,0 +1,71 @@
+import { groupBy } from 'lodash-es';
+
+import { loadI18nMessages } from 'helpers/i18n';
+
+import Head from 'components/head';
+import LayoutContainer from 'components/layout-container';
+import { StaticPageLayoutProps } from 'layouts/static-page';
+import { PageComponent } from 'types';
+import { GroupedEnums as GroupedEnumsType } from 'types/enums';
+import { Project as ProjectType } from 'types/project';
+
+import { getEnums } from 'services/enums/enumService';
+import { getProject } from 'services/projects/projectService';
+
+export const getServerSideProps = async ({ params: { id }, locale }) => {
+  let project;
+
+  // If getting the project fails, it's most likely because the record has
+  // not been found. Let's return a 404. Anything else will trigger a 500 by default.
+  try {
+    ({ data: project } = await getProject(id));
+  } catch (e) {
+    return { notFound: true };
+  }
+
+  const enums = await getEnums();
+
+  return {
+    props: {
+      intlMessages: await loadI18nMessages({ locale }),
+      enums: groupBy(enums, 'type'),
+      project: project,
+    },
+  };
+};
+
+type ProjectPageProps = {
+  project: ProjectType;
+  enums: GroupedEnumsType;
+};
+
+const ProjectPage: PageComponent<ProjectPageProps, StaticPageLayoutProps> = ({
+  project,
+  enums,
+}) => {
+  return (
+    <>
+      <Head title={project.name} description={project.description} />
+
+      <LayoutContainer className="-mt-10 lg:-mt-16">
+        {/*<ProjectHeader/>*/} ProjectHeader
+      </LayoutContainer>
+
+      <LayoutContainer className="mb-20 mt-18">
+        <section>Overview</section>
+        <section>Impact</section>
+        <section>Funding &amp; Development</section>
+      </LayoutContainer>
+
+      <div className="bg-background-middle">
+        <LayoutContainer className="py-18">
+          <section>Project developers</section>
+        </LayoutContainer>
+      </div>
+    </>
+  );
+};
+
+ProjectPage.layout = {};
+
+export default ProjectPage;
---
+++ frontend/services/projects/projectService.ts
@@ -0,0 +1,42 @@
+import { useMemo } from 'react';
+
+import { useQuery } from 'react-query';
+
+import { AxiosRequestConfig } from 'axios';
+
+import { Queries } from 'enums';
+import { Project } from 'types/project';
+
+import API from 'services/api';
+
+/** Get a Project using an id and, optionally, the wanted fields */
+export const getProject = async (
+  id: string,
+  params?: {
+    fields?: string;
+    includes?: string;
+  }
+): Promise<{
+  data: Project;
+  included: any[]; // TODO
+}> => {
+  const config: AxiosRequestConfig = {
+    url: `/api/v1/projects/${id}`,
+    method: 'GET',
+    params: params,
+  };
+  return await API.request(config).then((response) => response.data);
+};
+
+/** Use query for a single Project */
+export function useProject(id: string, params) {
+  const query = useQuery([Queries.Project, id], () => getProject(id, params));
+
+  return useMemo(
+    () => ({
+      ...query,
+      project: query.data,
+    }),
+    [query]
+  );
+}
