5b85a25bf593d7376df1f7ee72a11b5d27e74c0b
Tomasz Subik <tsubik@gmail.com>
2022-04-29 10:58:47 +0200

    temp disable origin check for rails controllers

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -15,6 +15,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
+  # TODO: fix this later, directupload is checkin it on staging but we want to allow non https localhost requests
+  config.action_controller.forgery_protection_origin_check = false
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
