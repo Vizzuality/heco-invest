59448799b4c5ac6b211381b8471d16fb2b136d9a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2022-05-03 10:18:10 +0200

    Simplyfy by using translatable_attributes instead of reinventing the wheel

--- backend/app/services/translations/translate_project.rb
+++ backend/app/services/translations/translate_project.rb
@@ -8,7 +8,7 @@ module Translations
       translate_content = TranslateContent.new(translatable_content, source_language_code)
       (Language::TYPES - [source_language_code]).each do |target_language_code|
         column_translations = translate_content.call(target_language_code)
-        present_translatable_columns.each.with_index do |col, idx|
+        present_translatable_attributes.each.with_index do |col, idx|
           I18n.with_locale(target_language_code) { @project.send(:"#{col}=", column_translations[idx]) }
         end
       end
@@ -19,30 +19,15 @@ module Translations
       @project.language
     end
 
-    def self.translatable_columns
-      [
-        :name,
-        :description,
-        :problem,
-        :solution,
-        :sustainability,
-        :expected_impact,
-        :funding_plan,
-        :replicability,
-        :progress_impact_tracking,
-        :relevant_links
-      ]
-    end
-
-    def present_translatable_columns
-      self.class.translatable_columns.select do |col|
+    def present_translatable_attributes
+      Project.translatable_attributes.select do |col|
         # check for presence of content, since we cannot send empty strings to translation API
         @project.send(:"#{col}?", locale: source_language_code)
       end
     end
 
     def translatable_content
-      present_translatable_columns.map do |col|
+      present_translatable_attributes.map do |col|
         @project.send(col, locale: source_language_code)
       end
     end
--- backend/spec/services/translations/translate_project_spec.rb
+++ backend/spec/services/translations/translate_project_spec.rb
@@ -2,15 +2,14 @@ require "rails_helper"
 
 RSpec.describe Translations::TranslateProject do
   let(:project) {
-    translatable_columns = Translations::TranslateProject.translatable_columns
-    attrs = translatable_columns.map { |col| ["#{col}_en", "test"] }.to_h
+    attrs = Project.translatable_attributes.map { |col| ["#{col}_en", "test"] }.to_h
     create(:project, attrs.merge(name_en: "Project name")) # for simplicity just one translatable attribute present
   }
   subject { described_class.new project }
 
   describe "#call" do
     before do
-      spanish_translations = Translations::TranslateProject.translatable_columns.map { "test" }
+      spanish_translations = Project.translatable_attributes.map { "test" }
       spanish_translations[0] = "Nombre del proyecto"
       allow(@translate_content_double).to receive(:call).with(any_args).and_return(spanish_translations)
       subject.call
