db69a2885533da1e0df3211c515ba845a4e29d98
Simão Rodrigues <SARodrigues@users.noreply.github.com>
2022-05-11 13:07:18 +0100

    Update bad DiscoverSearch incorrect type default
    Co-authored-by: Clément Prod'homme <clementprdhomme@users.noreply.github.com>

--- frontend/containers/discover-search/types.ts
+++ frontend/containers/discover-search/types.ts
@@ -1,5 +1,5 @@
 export type DiscoverSearchProps = {
-  /** Default value for the input. Defaults to `false` */
+  /** Default value for the input. Defaults to `''` */
   defaultValue?: string;
   /** Callback for when a search is performed */
   onSearch?: (searchText: string) => void;
