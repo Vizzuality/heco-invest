463914c54edd17390b106c49f7b7b6e9ed18ad45
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-05-11 13:17:07 +0100

    Improve BadgeNavigation styling

--- frontend/components/badge-navigation/component.tsx
+++ frontend/components/badge-navigation/component.tsx
@@ -19,20 +19,24 @@ export const BadgeNavigation: FC<BadgeNavigationProps> = ({
 
   return (
     <div className={className}>
-      <nav>
-        <ol className="flex gap-2 border rounded-full whitespace-nowrap border-beige">
+      <nav className="relative mx-2">
+        <span className="absolute left-0 right-0 border rounded-full top-2 bottom-2 border-beige" />
+        <ol className="flex gap-2 py-2 whitespace-nowrap">
           {items.map(({ id, name, link, number }) => {
             const isActive = id === activeId;
 
             return (
-              <li key={link}>
+              <li key={link} className="z-10 transition-all">
                 <Link href={link}>
-                  <a aria-current={isActive ? 'location' : false}>
+                  <a
+                    className="flex rounded-full focus-visible:outline focus-visible:outline-green-dark focus-visible:outline-2 focus-visible:outline-offset-2"
+                    aria-current={isActive ? 'location' : false}
+                  >
                     <Tag
                       className={cx({
-                        'text-sm transition-colors border': true,
-                        'bg-white font-semibold text-black': isActive,
-                        'text-green-dark border-transparent': !isActive,
+                        'text-sm hover:font-medium hover:text-black': true,
+                        'bg-white font-semibold text-black shadow-sm': isActive,
+                        'text-green-dark border-none p-px': !isActive,
                       })}
                     >
                       {name}
