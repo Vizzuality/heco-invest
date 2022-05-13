9b0b8cabd676c27e33cae95dba1294efbb2c2d1c
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-05-13 14:34:00 +0100

    Layouts now make use of the shared menus

--- frontend/layouts/discover-page/header/component.tsx
+++ frontend/layouts/discover-page/header/component.tsx
@@ -1,17 +1,14 @@
 import { FC } from 'react';
 
-import { useIntl } from 'react-intl';
-
 import Link from 'next/link';
 
 import LayoutContainer from 'components/layout-container';
 import NavigationMenuButton from 'layouts/shared/navigation-menu-button';
+import UserMenu from 'layouts/shared/user-menu';
 
 import { HeaderProps } from './types';
 
 export const Header: FC<HeaderProps> = ({}: HeaderProps) => {
-  const intl = useIntl();
-
   return (
     <>
       <header className="fixed top-0 z-10 w-full text-white border-b bg-green-dark backdrop-blur-sm">
@@ -22,7 +19,10 @@ export const Header: FC<HeaderProps> = ({}: HeaderProps) => {
             </Link>
             {/* Making space for the Search container in the layout, just in case */}
             <span className="max-w-3xl" />
-            <NavigationMenuButton />
+            <div className="flex items-center gap-2 lg:gap-4">
+              <UserMenu />
+              <NavigationMenuButton />
+            </div>
           </div>
         </LayoutContainer>
       </header>
--- frontend/layouts/static-page/header/component.tsx
+++ frontend/layouts/static-page/header/component.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 
-import { ChevronDown } from 'react-feather';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import cx from 'classnames';
@@ -12,16 +11,12 @@ import { useWindowScrollPosition } from 'rooks';
 
 import useMe from 'hooks/me';
 
-import { getUserInitials } from 'helpers/user';
-
 import ActiveLink from 'components/active-link';
-import Button from 'components/button';
 import Icon from 'components/icon';
 import LanguageSelector from 'components/language-selector';
 import LayoutContainer from 'components/layout-container';
-import Menu, { MenuItem, MenuSection } from 'components/menu';
-import { Paths } from 'enums';
 import NavigationMenuButton from 'layouts/shared/navigation-menu-button';
+import UserMenu from 'layouts/shared/user-menu';
 
 import { useCurrentProjectDeveloper } from 'services/project-developers/projectDevelopersService';
 
@@ -47,19 +42,6 @@ export const Header: React.FC<HeaderProps> = ({
     // eslint-disable-next-line react-hooks/rules-of-hooks
     typeof window === 'undefined' ? { scrollY: 0, scrollX: 0 } : useWindowScrollPosition();
   const showBackground = !transparent || scrollY > 0;
-
-  const [userMenuOpen, setUserMenuOpen] = useState(false);
-
-  const onClickMenuItem = useCallback(
-    (key) => {
-      switch (key) {
-        default:
-          router.push(key);
-          break;
-      }
-    },
-    [router]
-  );
 
   return (
     <header
@@ -102,98 +84,17 @@ export const Header: React.FC<HeaderProps> = ({
               </nav>
             </div>
 
-            {/* SELECT LANGUAGE MENU */}
-            <div className="self-center">
+            <div className="flex items-center gap-1 lg:gap-2">
               <LanguageSelector />
+              <UserMenu
+                className="hidden sm:flex"
+                theme={showBackground ? 'primary-green' : 'primary-white'}
+              />
+              <NavigationMenuButton
+                className="flex lg:hidden"
+                theme={showBackground ? 'primary-green' : 'primary-white'}
+              />
             </div>
-
-            {/* USER MENU (MD/LG)*/}
-            <div className="items-center hidden sm:flex">
-              {!!user ? (
-                <Menu
-                  Trigger={
-                    <Button
-                      aria-label={formatMessage({ defaultMessage: 'User menu', id: '6b2CRH' })}
-                      className="px-2 sm:px-4 focus-visible:outline-green-dark"
-                      theme="naked"
-                      size="small"
-                      aria-expanded={userMenuOpen}
-                      onClick={() => setUserMenuOpen(true)}
-                    >
-                      <span
-                        className={cx('w-8 h-8 rounded-full flex justify-center items-center', {
-                          'bg-green-dark text-white': showBackground,
-                          'text-green-dark bg-white': !showBackground,
-                        })}
-                      >
-                        {getUserInitials(user)}
-                      </span>
-                      <ChevronDown className="inline-block w-4 h-4 ml-1" />
-                    </Button>
-                  }
-                  onAction={onClickMenuItem}
-                  align="end"
-                  direction="bottom"
-                  className="md:p-4 md:py-1 lg:pr-0"
-                  header={
-                    <div className="flex">
-                      <div className="flex items-center justify-center w-12 h-12 mr-2 text-white rounded-full bg-green-dark">
-                        {getUserInitials(user)}
-                      </div>
-                      <div className="pb-2 pl-2 pr-2 border-b border-bg-dark">
-                        <span className="text-green-dark">
-                          {user.first_name} {user.last_name}
-                        </span>
-                        <span className="block text-gray-400">{user.email}</span>
-                      </div>
-                    </div>
-                  }
-                >
-                  <MenuSection>
-                    {!!account && (
-                      <MenuItem
-                        key={Paths.Dashboard}
-                        textValue={formatMessage(
-                          {
-                            defaultMessage: '{accountName} account',
-                            id: 'YE6fVO',
-                          },
-                          { accountName: account.name }
-                        )}
-                      >
-                        <FormattedMessage
-                          defaultMessage="{accountName} account"
-                          id="YE6fVO"
-                          values={{
-                            accountName: account.name,
-                          }}
-                        />
-                      </MenuItem>
-                    )}
-                    <MenuItem key={Paths.Settings}>
-                      <FormattedMessage defaultMessage="My preferences" id="CEQo2w" />{' '}
-                    </MenuItem>
-                    <MenuItem key={Paths.SignOut}>
-                      <FormattedMessage defaultMessage="Sign out" id="xXbJso" />
-                    </MenuItem>
-                  </MenuSection>
-                </Menu>
-              ) : (
-                <Button
-                  theme={showBackground ? 'primary-green' : 'primary-white'}
-                  size="small"
-                  className="hidden lg:block"
-                  to={Paths.SignIn}
-                >
-                  <FormattedMessage defaultMessage="Sign in" id="SQJto2" />
-                </Button>
-              )}
-            </div>
-
-            <NavigationMenuButton
-              className="flex lg:hidden"
-              theme={showBackground ? 'primary-green' : 'primary-white'}
-            />
           </div>
         </div>
       </LayoutContainer>
