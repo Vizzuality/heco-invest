d22e08dca1d076d0a796a8618dd399d593db73f3
martintomas <martintomas.it@gmail.com>
2022-05-16 17:15:12 +0200

    fix: use BACKEND_URL port and protocol for routes.default_url_options

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -11,7 +11,12 @@ module Backend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    Rails.application.routes.default_url_options[:host] = URI.parse(ENV.fetch("BACKEND_URL", "http://localhost:4000")).host
+    backend_url = URI.parse ENV.fetch("BACKEND_URL", "http://localhost:4000")
+    Rails.application.routes.default_url_options = {
+      host: backend_url.host,
+      port: backend_url.port,
+      protocol: backend_url.scheme
+    }
 
     config.generators.test_framework = :rspec
 
