a2eef8a3ffbc886f757dc07daf4bc04afa405c3e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2022-05-18 18:18:23 +0200

    Try another way to set the correct queue name

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -41,7 +41,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :google
-  config.active_storage.queue = ENV["CLOUD_TASKS_TEST_QUEUE_NAME"].to_sym
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -65,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "backend_production"
   config.active_job.queue_adapter = :cloudtasker
+  config.active_job.default_queue_name = ENV["CLOUD_TASKS_TEST_QUEUE_NAME"].to_sym
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
