97488cc322a128bc4e3714e639da04f7149bbe4a
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2022-05-19 17:38:57 +0200

    Not sure we need this, fails with 'Missing host to link to' otherwise

--- backend/config/environments/test.rb
+++ backend/config/environments/test.rb
@@ -61,3 +61,6 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :test
 end
+
+# Set the default host and port to be the same as Action Mailer.
+Rails.application.default_url_options = Rails.application.config.action_mailer.default_url_options
