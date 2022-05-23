30a703c3588e625213930ae3ebcaaef07755c1b3
Bárbara Chaves <barbarabarcellos90@gmail.com>
2022-05-23 16:53:35 +0200

    Update frontend/components/carousel/types.ts
    Co-authored-by: Clément Prod'homme <clementprdhomme@users.noreply.github.com>

--- frontend/components/carousel/types.ts
+++ frontend/components/carousel/types.ts
@@ -1,6 +1,6 @@
 export type CarouselProps = React.PropsWithChildren<{
   /** Classnames to apply the wrapper element*/
   className?: string;
-  /** Default slide index */
+  /** Initial slide index. Default to `0`. */
   defaultSlide?: number;
 }>;
