5922f1b02597b8cb1a24101144f32fd2ccf6b5ee
anamontiaga <amontiaga@gmail.com>
2022-05-24 11:51:54 +0200

    project page: basic overview component

---
+++ frontend/layouts/project-page/component.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { useRouter } from 'next/router';
+
+import useMe from 'hooks/me';
+
+import { Paths, UserRoles } from 'enums';
+import { ProjectProps } from 'layouts/project-page/types';
+
+const Project: React.FC<ProjectProps> = ({ permissions, children, ...rest }) => {
+  const router = useRouter();
+  const { user, isLoading, isError } = useMe();
+
+  // Not display anything when me request is on progress
+  if (isLoading) return null;
+
+  // Redirect to sign-in when session doesn't exist
+  if (isError) {
+    router.push(Paths.SignIn);
+    return null;
+  }
+
+  if (!permissions.includes(user.role)) {
+    if (user.role === UserRoles.Light) {
+      // Redirect to choose account type if the user have a Light account
+      router.push(Paths.AccountType);
+      return null;
+    }
+    // Redirect to the last route (go back) if the user have a different kind of account
+    router.back();
+    return null;
+  }
+
+  return <div {...rest}>{children}</div>;
+};
+
+export default Project;
