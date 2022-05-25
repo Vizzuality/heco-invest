ccab0ff3c70cd85de54547a76239a3996d25d125
Tomasz Subik <tsubik@gmail.com>
2022-05-25 16:13:27 +0200

    reject/approve was moved to accounts_controller

--- backend/app/controllers/backoffice/project_developers_controller.rb
+++ backend/app/controllers/backoffice/project_developers_controller.rb
@@ -1,25 +1,7 @@
 module Backoffice
   class ProjectDevelopersController < BaseController
-    before_action :fetch_project_developer, only: [:approve, :reject]
-
     def index
-      @project_developers = ProjectDeveloper.all
-    end
-
-    def approve
-      @project_developer.account.approved!
-      redirect_back(fallback_location: admin_root_path)
-    end
-
-    def reject
-      @project_developer.account.rejected!
-      redirect_back(fallback_location: admin_root_path)
-    end
-
-    private
-
-    def fetch_project_developer
-      @project_developer = ProjectDeveloper.find(params[:id])
+      @project_developers = ProjectDeveloper.all.includes(account: [:owner])
     end
   end
 end
