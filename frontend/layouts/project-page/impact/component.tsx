316da5151f28ddd897e5bc28992e12aa3acaeae7
anamontiaga <amontiaga@gmail.com>
2022-05-25 18:05:16 +0200

    project public page: add impact basics section

--- frontend/layouts/project-page/impact/component.tsx
+++ frontend/layouts/project-page/impact/component.tsx
@@ -1,28 +1,137 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import { useForm } from 'react-hook-form';
 import { FormattedMessage } from 'react-intl';
 
+import SDGs from 'containers/sdgs/component';
+
+import Combobox, { Option } from 'components/forms/combobox';
 import ImpactChart from 'components/impact-chart';
 import LayoutContainer from 'components/layout-container';
+import Tag from 'components/tag';
 import { ImpactProps } from 'layouts/project-page/impact/types';
+import sdgsMock from 'mockups/sdgs.json';
+import { Enum } from 'types/enums';
 
 export const Impact: React.FC<ImpactProps> = ({ project }: ImpactProps) => {
+  const [impactLocation, setImpactLocation] = useState('Municipality');
+  console.log(impactLocation);
   // TODO: Change for real project attibute
   const projectImpact = [4, 6, 8, 3];
+  console.log({ project });
+  const { target_groups: tags, expected_impact: expected } = project;
+  const sdgs = sdgsMock.filter(({ id }) => project.sdgs.includes(parseInt(id)));
+  const formatCapitalizeDashedString = (s) => {
+    return s.charAt(0).toUpperCase() + s.slice(1).replace(/-/g, ' ');
+  };
+  const formatDashedString = (s) => {
+    return s.replace(/-/g, ' ');
+  };
+  const { control } = useForm();
+
+  const OPTIONS = [
+    { key: 'municipality', label: 'Municipality' },
+    { key: 'hydrobasin', label: 'Hydrobasin' },
+    { key: 'heco-priority-landscape', label: 'HeCo priority landscape' },
+  ];
+
   return (
     <section>
       <LayoutContainer className="mb-20 space-y-6 mt-36">
         <h2 className="pl-16 font-serif text-3xl text-black">
           <FormattedMessage defaultMessage="Impact" id="W2JBdp" />
         </h2>
-        <div className="px-2 py-16 lg:justify-between sm:px-12 sm:py-20 lg:flex bg-background-greenLight rounded-2xl">
-          <div>
-            <h2>
-              <FormattedMessage defaultMessage="Estimated Impact" id="PheCRL" />
-            </h2>
+        <div className="flex-col px-2 py-16 space-y-24 font-sans lg:justify-between sm:px-12 sm:py-20 lg:flex bg-background-greenLight rounded-2xl">
+          <div className="flex justify-between space-x-4">
+            <div className="flex flex-col w-1/2 space-y-6">
+              <div className="flex flex-col space-y-6">
+                <h3 className="text-xl font-semibold">
+                  <FormattedMessage defaultMessage="Target group(s)" id="ilgFAX" />
+                </h3>
+                <div>
+                  {tags.map((tag) => (
+                    <Tag key={tag} className="mb-2 mr-2 text-sm text-black bg-white" size="small">
+                      {formatCapitalizeDashedString(tag)}
+                    </Tag>
+                  ))}
+                </div>
+              </div>
+              <div className="flex flex-col soace-y-6">
+                <h3 className="text-xl font-semibold">
+                  <FormattedMessage defaultMessage="Expected Impact" id="8rIwwr" />
+                </h3>
+                <p className="text-base">{expected}</p>
+              </div>
+            </div>
+            <div className="flex flex-col w-1/2 space-y-6">
+              <h3 className="text-xl font-semibold">
+                <FormattedMessage defaultMessage="SDG'S" id="vTJ8GJ" />
+              </h3>
+              <SDGs sdgs={sdgs as Enum[]} size="large" />
+            </div>
           </div>
-          <div className="py-10 pr-24 pl-28">
-            <ImpactChart category={project.category} impact={projectImpact} />
+
+          <div className="flex justify-between h-full">
+            <div className="flex flex-col justify-between">
+              <div className="flex flex-col items-start w-3/4 pr-24 space-y-6">
+                <h3 className="text-xl font-semibold">
+                  <FormattedMessage defaultMessage="Estimated Impact" id="PheCRL" />
+                </h3>
+                <div className="flex flex-col space-y-2.5 w-full">
+                  <label htmlFor="impact-location" className="text-sm text-gray-800">
+                    <FormattedMessage defaultMessage="Display impact on" id="ufxVRM" />
+                  </label>
+                  <Combobox
+                    id="impact-location"
+                    name="impact-location"
+                    aria-describedby="country-error"
+                    control={control}
+                    controlOptions={{
+                      disabled: false,
+                      onChange: (e) => setImpactLocation(e.target.value),
+                    }}
+                  >
+                    {OPTIONS.map(({ key, label }) => (
+                      <Option key={key}>{label}</Option>
+                    ))}
+                  </Combobox>
+                </div>
+                <p className="py-4">
+                  <FormattedMessage
+                    defaultMessage="In the {impactLocation} the the project has higher impact on Water
+                    and has an impact score of 30."
+                    id="eVJMyI"
+                    values={{ impactLocation: formatDashedString(impactLocation), score: 30 }}
+                  />
+                </p>
+                <button
+                  type="button"
+                  className="underline hover:no-underline text-green-dark pointer"
+                >
+                  <FormattedMessage defaultMessage="How is the impact calculated?" id="9cE0nR" />
+                </button>
+              </div>
+              <div className="flex flex-col items-center p-6 font-semibold bg-white w-52 rounded-xl">
+                <p className=" text-green-dark">
+                  <span className="text-2xl">30</span>/ 100
+                </p>
+                <div className="flex items-center space-x-2">
+                  <p className="text-base text-gray-800">
+                    <FormattedMessage defaultMessage="Impact score" id="2GBpne" />
+                  </p>
+                  <button
+                    type="button"
+                    className="box-border flex items-center justify-center w-4 h-4 text-gray-800 border border-gray-800 rounded-full pointer"
+                  >
+                    <p className="text-xs">i</p>
+                  </button>
+                </div>
+              </div>
+              {/* TODO: Add Impact Modal (previously should be componentizated) */}
+            </div>
+            <div className="w-1/2 my-24">
+              <ImpactChart category={project.category} impact={projectImpact} />
+            </div>
           </div>
         </div>
       </LayoutContainer>
