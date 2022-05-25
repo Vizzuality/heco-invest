f9bc6e40d103b19e89d0efdd6edea7bae4616092
Tomasz Subik <tsubik@gmail.com>
2022-05-25 15:21:50 +0200

    backoffice: add simple navbar

--- backend/app/helpers/application_helper.rb
+++ backend/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_link_to(text, path)
+    is_active = current_page?(path)
+    classnames = {
+      "text-beige hover:text-green-light": true,
+      "font-bold": is_active
+    }.reject { |_k, v| v == false }.keys
+
+    link_to text, path, class: classnames
+  end
 end
