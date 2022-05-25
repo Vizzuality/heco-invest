9995475ec40279575e04c7e3277e4952ee939cd9
Tomasz Subik <tsubik@gmail.com>
2022-05-25 14:50:55 +0200

    approve/reject accounts

--- backend/app/controllers/backoffice/project_developers_controller.rb
+++ backend/app/controllers/backoffice/project_developers_controller.rb
@@ -1,7 +1,25 @@
 module Backoffice
   class ProjectDevelopersController < BaseController
+    before_action :fetch_project_developer, only: [:approve, :reject]
+
     def index
       @project_developers = ProjectDeveloper.all
+    end
+
+    def approve
+      @project_developer.account.approved!
+      redirect_back(fallback_location: admin_root_path)
+    end
+
+    def reject
+      @project_developer.account.rejected!
+      redirect_back(fallback_location: admin_root_path)
+    end
+
+    private
+
+    def fetch_project_developer
+      @project_developer = ProjectDeveloper.find(params[:id])
     end
   end
 end
--- backend/config/routes/backoffice.rb
+++ backend/config/routes/backoffice.rb
@@ -3,5 +3,11 @@ devise_for :admins, path: "backoffice"
 get "/backoffice", to: "backoffice/dashboards#show", as: :admin_root
 
 namespace :backoffice do
+  resources :accounts, only: [] do
+    member do
+      post :approve
+      post :reject
+    end
+  end
   resources :project_developers, only: [:index]
 end
