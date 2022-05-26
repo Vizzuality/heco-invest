3d1a94da42104cd6b9e252dc41079065a3a9b7e1
martintomas <martintomas.it@gmail.com>
2022-05-26 12:16:06 +0200

    feat: Adding priority landscape to Projects

--- backend/app/serializers/api/v1/project_serializer.rb
+++ backend/app/serializers/api/v1/project_serializer.rb
@@ -48,6 +48,7 @@ module API
       belongs_to :country, serializer: LocationSerializer
       belongs_to :municipality, serializer: LocationSerializer
       belongs_to :department, serializer: LocationSerializer
+      belongs_to :priority_landscape, serializer: LocationSerializer
       has_many :involved_project_developers, serializer: ProjectDeveloperSerializer
       has_many :project_images
 
