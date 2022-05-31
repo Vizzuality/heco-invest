c8c96f40816f9e7b40bb23cd2a67480fd6c08598
martintomas <martintomas.it@gmail.com>
2022-05-31 17:55:28 +0200

    refactoring: simplification of pagination design

--- backend/spec/support/shared_examples/backoffice/with_table_pagination.rb
+++ backend/spec/support/shared_examples/backoffice/with_table_pagination.rb
@@ -1,8 +1,9 @@
 RSpec.shared_examples "with table pagination" do
   describe "Paginated" do
     it "shows page navigation" do
-      expect(page).to have_text(t("backoffice.common.navigation.prev"))
-      expect(page).to have_text(t("backoffice.common.navigation.next"))
+      within ".pagination .active" do
+        expect(page).to have_text("1")
+      end
     end
   end
 end
