7f2b5556c628f20663d0ecdaff74f00dbb472d5b
martintomas <martintomas.it@gmail.com>
2022-06-01 15:02:34 +0200

    feat: Making backoffice tables sortable

--- backend/app/controllers/backoffice/investors_controller.rb
+++ backend/app/controllers/backoffice/investors_controller.rb
@@ -1,8 +1,8 @@
 module Backoffice
   class InvestorsController < BaseController
     def index
-      @investors = Investor.all.includes(account: [:owner])
-      @pagy_object, @investors = pagy @investors
+      @q = Investor.ransack params[:q]
+      @pagy_object, @investors = pagy @q.result.includes(account: [:owner])
     end
   end
 end
