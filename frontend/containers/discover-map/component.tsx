5e8541286509bd7a5caab8db838c1a8735bb0857
barbara-chaves <barbarabarcellos90@gmail.com>
2022-06-01 10:01:15 +0200

    test map

--- frontend/containers/discover-map/component.tsx
+++ frontend/containers/discover-map/component.tsx
@@ -1,36 +1,111 @@
 import { FC, useState } from 'react';
 
-import { Marker } from 'react-map-gl';
-import MapPin from './pin';
+import { Marker, Source } from 'react-map-gl';
+import type { LayerProps } from 'react-map-gl';
+
 import Image from 'next/image';
 
 import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
-import { LayerManager } from '@vizzuality/layer-manager-react';
+import CartoProvider from '@vizzuality/layer-manager-provider-carto';
+import { LayerManager, Layer } from '@vizzuality/layer-manager-react';
 
-import mockupData from 'mockups/projects-map.json';
+import { CategoryTagDot } from 'containers/category-tag';
 
+import Icon from 'components/icon';
 import Map from 'components/map';
 import Controls from 'components/map/controls';
+import LAYERS from 'components/map/layers';
 import ProjectLegend from 'components/map/project-legend';
+import mockupData from 'mockups/projects-map.json';
+import { CategoryType } from 'types/category';
 
 import { useProjectsMap } from 'services/projects/projectService';
 
-import { DiscoverMapProps } from './types';
-import { CategoryTagDot } from 'containers/category-tag';
-import { CategoryType } from 'types/category';
-import Icon from 'components/icon';
-
 import MarkerIcon from 'svgs/map/marker.svg';
 
+import MapPin from './pin';
+import { DiscoverMapProps } from './types';
+
+export const clusterLayer: LayerProps = {
+  id: 'clusters',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['has', 'point_count'],
+  paint: {
+    'circle-color': ['step', ['get', 'point_count'], '#51bbd6', 100, '#f1f075', 750, '#f28cb1'],
+    'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40],
+  },
+};
+
+export const clusterCountLayer: LayerProps = {
+  id: 'cluster-count',
+  type: 'symbol',
+  source: 'earthquakes',
+  paint: {},
+  filter: ['has', 'point_count'],
+  layout: {
+    'text-field': '{point_count_abbreviated}',
+    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+    'text-size': 12,
+  },
+};
+
+export const unclusteredPointLayer: LayerProps = {
+  id: 'unclustered-point',
+  type: 'circle',
+  source: 'earthquakes',
+  filter: ['!', ['has', 'point_count']],
+  paint: {
+    'circle-color': '#11b4da',
+    'circle-radius': 4,
+    'circle-stroke-width': 1,
+    'circle-stroke-color': '#fff',
+  },
+};
+
+const layers = [clusterLayer, clusterCountLayer, unclusteredPointLayer];
+
+const layer = {
+  id: 'test',
+  type: 'geojson',
+  params: {
+    color: '#CCC',
+  },
+  source: {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: mockupData.data.map((r) => ({
+        type: 'Feature',
+        properties: r,
+        geometry: {
+          type: 'Polygon',
+          coordinates: [r.longitude, r.latitude],
+        },
+      })),
+    },
+    cluster: true,
+    clusterMaxZoom: 4,
+    clusterRadius: 50,
+  },
+  render: {
+    metadata: {
+      position: 'top',
+    },
+    layers,
+  },
+};
+
 export const DiscoverMap: FC<DiscoverMapProps> = () => {
+  const cartoProvider = new CartoProvider();
   const [viewport, setViewport] = useState({});
   const [bounds, setBounds] = useState({
     bbox: [-81.99, -4.35, -65.69, 12.54],
     options: { padding: 0 },
   });
 
-  const { projectsMap } = useProjectsMap({});
-  console.log(projectsMap, mockupData.data);
+  // const { projectsMap } = useProjectsMap({});
+  // console.log(projectsMap, mockupData.data);
 
   return (
     <div className="relative w-full h-full">
@@ -42,10 +117,29 @@ export const DiscoverMap: FC<DiscoverMapProps> = () => {
       >
         {(map) => (
           <>
-            {mockupData.data.map((projectMap) => (
-              <MapPin key={projectMap.id} projectMap={projectMap} />
-            ))}
-            <LayerManager map={map} plugin={PluginMapboxGl}></LayerManager>
+            {/* <Source
+              id="earthquakes"
+              type="geojson"
+              data="https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson"
+              cluster={true}
+              clusterMaxZoom={14}
+              clusterRadius={50}
+            >
+              <Layer {...clusterLayer} />
+              <Layer {...clusterCountLayer} />
+              <Layer {...unclusteredPointLayer} />
+            </Source> */}
+            <LayerManager
+              map={map}
+              plugin={PluginMapboxGl}
+              // providers={{
+              //   [cartoProvider.name]: cartoProvider.handleData,
+              // }}
+            >
+              {[layer].map((l) => (
+                <Layer key={l.id} {...l} />
+              ))}
+            </LayerManager>
           </>
         )}
       </Map>
