5068a9a87f0f67c185565e8cdc318188f0f910e1
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2022-06-01 12:14:22 +0200

    Use error handling in parent class instead

--- backend/app/jobs/translate_job.rb
+++ backend/app/jobs/translate_job.rb
@@ -1,12 +1,6 @@
 class TranslateJob < ApplicationJob
   queue_as ENV["CLOUD_TASKS_TEST_QUEUE_NAME"].to_sym
 
-  rescue_from(ActiveRecord::RecordNotFound) do |exception|
-    Rails.logger.error(exception)
-    Rails.logger.error(Rails.backtrace_cleaner.clean(exception.backtrace).join("\n"))
-    Google::Cloud::ErrorReporting.report exception
-  end
-
   def perform(resource)
     Translations::Translate.new(resource).call
   end
