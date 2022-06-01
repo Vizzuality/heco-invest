acf81c66216e3534a0dbf5c430735af4edadb659
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-06-01 15:17:57 +0100

    Refactor the ImpactChart component to take an organized object of impacts instead of an array of values with unclear order

--- frontend/components/impact-chart/component.tsx
+++ frontend/components/impact-chart/component.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 
 import { Radar } from 'react-chartjs-2';
 
@@ -14,6 +14,7 @@ import {
 } from 'chart.js';
 
 import FieldInfo from 'components/forms/field-info';
+import { Impacts as ImpactsEnum } from 'enums';
 
 import { useEnums } from 'services/enums/enumService';
 
@@ -24,35 +25,69 @@ ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip);
 export const ImpactChart: FC<ImpactChartProps> = ({
   className,
   category,
-  impact = [],
+  impact,
   compactMode = false,
 }) => {
   const {
-    data: { impact: impacts, category: categories },
+    data: { impact: allImpacts, category: allCategories },
   } = useEnums();
+  // The object may contain Impacts ids, but we need to make sure we have all the values
+  // (biodiversity, climate, community, water) in order to display the chart. We also need
+  // the impacts data coming from the enums in order to display labels and descriptions
+  const isPlaceholder =
+    !impact ||
+    !allImpacts ||
+    !(Object.values(impact) || []).filter((value) => value !== null && !isNaN(value)).length;
 
-  //const impact = [];
-  const color = categories?.find(({ id }) => id === category)?.color;
+  const categoryColor = useMemo(
+    () => allCategories?.find(({ id }) => id === category)?.color,
+    [allCategories, category]
+  );
 
-  const isPlaceholder = !impact?.length;
+  const impactIds = useMemo(
+    () => [ImpactsEnum.Biodiversity, ImpactsEnum.Climate, ImpactsEnum.Community, ImpactsEnum.Water],
+    []
+  );
 
-  const data: ChartData<'radar'> = {
-    labels: ['', '', '', ''],
-    datasets: [
-      {
-        data: impact,
-        backgroundColor: color,
-        borderColor: color,
-        borderJoinStyle: 'round',
-        borderWidth: 1,
-        tension: 0.25,
-        pointBackgroundColor: 'rgba(255, 159, 64, 0)',
-        pointBorderColor: 'rgba(255, 159, 64, 0)',
-      },
-    ],
-  };
+  const impactData = useMemo(() => {
+    if (!allImpacts) return [];
 
-  const options: ChartOptions<'radar'> = {
+    return impactIds.reduce((acc, impactId) => {
+      const data = allImpacts.find(({ id }) => id === impactId);
+      if (!data) return;
+
+      return {
+        ...acc,
+        [impactId]: {
+          id: impactId,
+          value: impact && impact[impactId],
+          name: data.name,
+          description: data.description,
+        },
+      };
+    }, []);
+  }, [impactIds, allImpacts, impact]);
+
+  const chartData: ChartData<'radar'> = useMemo(
+    () => ({
+      labels: Array(impactIds.length).fill(''),
+      datasets: [
+        {
+          data: impactIds.reduce((acc, impactId) => [...acc, impactData[impactId]?.value || 0], []),
+          backgroundColor: categoryColor,
+          borderColor: categoryColor,
+          borderJoinStyle: 'round',
+          borderWidth: 1,
+          tension: 0.25,
+          pointBackgroundColor: 'rgba(255, 159, 64, 0)',
+          pointBorderColor: 'rgba(255, 159, 64, 0)',
+        },
+      ],
+    }),
+    [impactIds, categoryColor, impactData]
+  );
+
+  const chartOptions: ChartOptions<'radar'> = {
     scales: {
       r: {
         display: !compactMode,
@@ -96,9 +131,9 @@ export const ImpactChart: FC<ImpactChartProps> = ({
     return (
       <div className={className}>
         {isPlaceholder ? (
-          <div className="w-full aspect-square border border-dashed rounded-full bg-background-middle border-background-dark bg-radial-beige" />
+          <div className="w-full border border-dashed rounded-full aspect-square bg-background-middle border-background-dark bg-radial-beige" />
         ) : (
-          <Radar data={data} options={options} />
+          <Radar data={chartData} options={chartOptions} />
         )}
       </div>
     );
@@ -108,43 +143,43 @@ export const ImpactChart: FC<ImpactChartProps> = ({
     <div className={className}>
       <div className="flex justify-between w-full ">
         <div className="z-0 flex flex-col w-full h-full gap-2">
-          {impacts?.length && (
+          {allImpacts && (
             <span className="flex items-center justify-center w-full">
               <span className="hidden mr-2 text-sm font-semibold text-gray-800 sm:flex">
-                {impacts[0].name}
+                {impactData[ImpactsEnum.Biodiversity].name}
               </span>
-              <FieldInfo infoText={impacts[0].description} />
+              <FieldInfo infoText={impactData[ImpactsEnum.Biodiversity].description} />
             </span>
           )}
           <span className="flex w-full">
-            {impacts?.length && (
+            {allImpacts && (
               <span className="flex items-center justify-end mr-2">
                 <span className="hidden mr-2 text-sm font-semibold text-gray-800 sm:flex">
-                  {impacts[2].name}
+                  {impactData[ImpactsEnum.Water].name}
                 </span>
-                <FieldInfo infoText={impacts[2].description} />
+                <FieldInfo infoText={impactData[ImpactsEnum.Water].description} />
               </span>
             )}
             <span className="relative flex items-center w-full overflow-x-scroll aspect-square">
-              <span className="absolute top-2 right-2 bottom-2 left-2 bg-background-middle rounded-full opacity-50" />
-              <span className="absolute top-2 right-2 bottom-2 left-2 bg-background-middle rounded-full opacity-60 scale-50" />
-              <Radar className="z-10" data={data} options={options} />
+              <span className="absolute rounded-full opacity-50 top-2 right-2 bottom-2 left-2 bg-background-middle" />
+              <span className="absolute scale-50 rounded-full top-2 right-2 bottom-2 left-2 bg-background-middle opacity-60" />
+              <Radar className="z-10" data={chartData} options={chartOptions} />
             </span>
-            {impacts?.length && (
+            {allImpacts && (
               <span className="flex items-center justify-start ml-2">
                 <span className="hidden mr-2 text-sm font-semibold text-gray-800 sm:flex">
-                  {impacts[1].name}
+                  {impactData[ImpactsEnum.Climate].name}
                 </span>
-                <FieldInfo infoText={impacts[1].description} />
+                <FieldInfo infoText={impactData[ImpactsEnum.Climate].description} />
               </span>
             )}
           </span>
-          {impacts?.length && (
+          {allImpacts && (
             <span className="flex items-center justify-center w-full">
               <span className="hidden mr-2 text-sm font-semibold text-gray-800 sm:flex">
-                {impacts[3].name}
+                {impactData[ImpactsEnum.Community].name}
               </span>
-              <FieldInfo infoText={impacts[3].description} />
+              <FieldInfo infoText={impactData[ImpactsEnum.Community].description} />
             </span>
           )}
         </div>
