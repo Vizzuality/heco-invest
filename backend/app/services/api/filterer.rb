e04df97ae4c05c007ebe03f84cda07b8e05181d1
martintomas <martintomas.it@gmail.com>
2022-06-02 10:07:58 +0200

    feat: New numerical filter for Project impacts

--- backend/app/services/api/filterer.rb
+++ backend/app/services/api/filterer.rb
@@ -7,22 +7,30 @@ module API
       ProjectDeveloper => :account,
       Investor => :account
     }
+    NUMERICAL_FILTERS = %i[municipality_biodiversity_impact municipality_climate_impact municipality_water_impact municipality_community_impact]
     ENUM_FILTERS = %i[category impact sdg instrument_type ticket_size]
 
     def initialize(query, filters, language: I18n.locale)
       @query = query
       @filters = filters
       @language = language
+
+      expand_filters
     end
 
     def call
       apply_full_text_filter
+      apply_numerical_filter
       filter_by_enums
       filter_by_only_verified
       query
     end
 
     private
+
+    def expand_filters
+      filters[:impact].to_s.split(",").each { |i| filters["municipality_#{i}_impact"] = 0 }
+    end
 
     def apply_full_text_filter
       return query if filters[:full_text].blank?
@@ -31,6 +39,13 @@ module API
         full_text_record_ids_for relation.to_s.classify.constantize, attr: "#{relation}_id"
       end.flatten
       self.query = query.where(id: extra_ids + full_text_record_ids_for(query.klass))
+    end
+
+    def apply_numerical_filter
+      sql_fragments = filters.slice(*NUMERICAL_FILTERS.map(&:to_s)).slice(*column_names).map do |filter_key, filter_value|
+        ActiveRecord::Base.sanitize_sql ["#{filter_key} > ?", filter_value]
+      end
+      self.query = query.where sql_fragments.join(" OR ") if sql_fragments.present?
     end
 
     def filter_by_enums
