ff83e9d764d8113c2be960d322746eb9f8ee14c1
Tomasz Subik <tsubik@gmail.com>
2022-06-03 12:27:10 +0200

    add missing test mosaic

--- backend/spec/system/backoffice/projects_spec.rb
+++ backend/spec/system/backoffice/projects_spec.rb
@@ -3,11 +3,13 @@ require "system_helper"
 RSpec.describe "Backoffice: Projects", type: :system do
   let(:admin) { create(:admin, email: "admin@example.com", password: "SuperSecret6", first_name: "Admin", last_name: "Example") }
   let!(:project) {
-    create(
+    project = create(
       :project,
       name: "Project ultra name",
       project_developer: create(:project_developer, account: create(:account, name: "Ultra project developer name"))
     )
+    project.update(priority_landscape: create(:location, location_type: "region", name: "Test mosaic"))
+    project
   }
   let!(:projects) { create_list(:project, 4) }
 
@@ -29,6 +31,7 @@ RSpec.describe "Backoffice: Projects", type: :system do
       within_row("Project ultra name") do
         expect(page).to have_text("Ultra project developer name")
         expect(page).to have_text(Category.find(project.category).name)
+        expect(page).to have_text("Test mosaic")
         expect(page).to have_text("draft")
       end
     end
