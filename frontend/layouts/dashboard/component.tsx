5d71b6b9656befca4b52a630f887b3a51da533e1
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-06-06 13:19:40 +0100

    Fix minor Dashboard layout color issues

--- frontend/layouts/dashboard/component.tsx
+++ frontend/layouts/dashboard/component.tsx
@@ -32,30 +32,32 @@ export const DashboardLayout: FC<DashboardLayoutProps> = ({
 
   return (
     <ProtectedPage permissions={[UserRoles.ProjectDeveloper, UserRoles.Investor]}>
-      <div className="flex flex-col lg:h-screen">
-        <div className="flex flex-col bg-radial-green-dark bg-green-dark lg:backdrop-blur-sm">
-          <Header />
-          <LayoutContainer className="mt-18 lg:mt-0">
-            <div className="flex flex-col w-full text-white lg:flex-row">
-              <div className="flex flex-grow gap-8">
-                <div className="lg:translate-y-5">
-                  <AccountPicture name={accountData?.name} picture={accountData?.picture.small} />
+      <div className="min-h-screen bg-background-dark">
+        <div className="flex flex-col lg:h-screen ">
+          <div className="flex flex-col bg-radial-green-dark bg-green-dark lg:backdrop-blur-sm">
+            <Header />
+            <LayoutContainer className="mt-18 lg:mt-0">
+              <div className="flex flex-col w-full text-white lg:flex-row">
+                <div className="flex flex-grow gap-8">
+                  <div className="lg:translate-y-5">
+                    <AccountPicture name={accountData?.name} picture={accountData?.picture.small} />
+                  </div>
+                  <div className="flex flex-col justify-end pb-2 mt-5 lg:mt-0">
+                    <AccountInfo userRole={user?.role} account={accountData} />
+                    {breakpoint('lg') && <Navigation userRole={user?.role} />}
+                  </div>
                 </div>
-                <div className="flex flex-col justify-end pb-2 mt-5 lg:mt-0">
-                  <AccountInfo userRole={user?.role} account={accountData} />
-                  {breakpoint('lg') && <Navigation userRole={user?.role} />}
+                {!breakpoint('lg') && <Navigation className="mt-2" userRole={user?.role} />}
+                <div className="flex items-end justify-center">
+                  <div className="flex gap-2 my-4 lg:my-0 lg:translate-y-5">{buttons}</div>
                 </div>
               </div>
-              {!breakpoint('lg') && <Navigation className="mt-2" userRole={user?.role} />}
-              <div className="flex items-end justify-center">
-                <div className="flex gap-2 my-4 lg:my-0 lg:translate-y-5">{buttons}</div>
-              </div>
-            </div>
-          </LayoutContainer>
+            </LayoutContainer>
+          </div>
+          <main className="h-full overflow-y-scroll bg-background-dark">
+            <LayoutContainer className="py-8">{children}</LayoutContainer>
+          </main>
         </div>
-        <main className="h-full overflow-y-scroll border bg-background-dark border-green-dark">
-          <LayoutContainer className="py-8">{children}</LayoutContainer>
-        </main>
       </div>
     </ProtectedPage>
   );
