068c314f550b7704cd2ca115b0e5ab4b831a449d
barbara-chaves <barbarabarcellos90@gmail.com>
2022-06-10 16:05:20 +0200

    Add project to project page map

---
+++ frontend/containers/project-page/overview/helpers.ts
@@ -0,0 +1,39 @@
+import { find } from 'lodash-es';
+
+import { ValidGeometryType } from 'containers/forms/geometry/types';
+
+import tw from 'tailwind.config.js';
+
+export const getLayer = (feature: ValidGeometryType, category: string) => {
+  const colors = tw.theme.colors.category;
+  const color = find(colors, (value, key) => category?.includes(key));
+  console.log(color);
+
+  return {
+    id: 'multipolygon',
+    type: 'geojson',
+    source: {
+      type: 'geojson',
+      data: feature,
+    },
+    render: {
+      layers: [
+        {
+          type: 'fill',
+          paint: {
+            'fill-color': color,
+            'fill-opacity': 0.5,
+          },
+        },
+        {
+          type: 'line',
+          paint: {
+            'line-color': color,
+            'line-opacity': 1,
+            'line-width': 1,
+          },
+        },
+      ],
+    },
+  };
+};
