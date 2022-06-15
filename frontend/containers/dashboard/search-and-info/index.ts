3ca2e2bc7aee749954d96dbacea7c5abdd3a7a82
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-06-15 15:30:06 +0100

    Add dashboard/SearchAndInfo component for use throughout the dashboard

---
+++ frontend/containers/dashboard/search-and-info/index.ts
@@ -0,0 +1,2 @@
+export { default } from './component';
+export type { SearchAndInfoProps } from './types';
---
+++ frontend/containers/dashboard/search-and-info/types.ts
@@ -0,0 +1,6 @@
+import { PropsWithChildren } from 'react';
+
+export type SearchAndInfoProps = PropsWithChildren<{
+  /** Classes to apply to the container */
+  className?: string;
+}>;
