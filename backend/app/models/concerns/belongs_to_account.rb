07a08446efb820bd3afc548ac4d6a8e6d7f5a6cb
Tomasz Subik <tsubik@gmail.com>
2022-06-21 13:24:17 +0200

    add includes parameter for account/investor and account/project_developer API endpoints

--- backend/app/models/concerns/belongs_to_account.rb
+++ backend/app/models/concerns/belongs_to_account.rb
@@ -4,7 +4,7 @@ module BelongsToAccount
   included do
     belongs_to :account
 
-    delegate :name, :slug, :owner_id, :picture, :about, :website, :instagram, :facebook, :linkedin, :twitter,
+    delegate :name, :slug, :owner_id, :owner, :picture, :about, :website, :instagram, :facebook, :linkedin, :twitter,
       :contact_email, :contact_phone, :review_status, :reviewed_at, :review_message, to: :account
 
     scope :approved, -> { joins(:account).where(account: {review_status: :approved}) }
