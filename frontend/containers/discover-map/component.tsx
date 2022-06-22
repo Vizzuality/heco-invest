863fbe4a9c485da510cb5ead39069ebc8929aebf
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-06-22 11:08:07 +0100

    Set up DiscoverMap to be able to display context layers

--- frontend/containers/discover-map/component.tsx
+++ frontend/containers/discover-map/component.tsx
@@ -4,8 +4,11 @@ import { useSelector } from 'react-redux';
 
 import { useRouter } from 'next/router';
 
-import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
-import { LayerManager } from '@vizzuality/layer-manager-react';
+import MapboxGLPlugin from '@vizzuality/layer-manager-plugin-mapboxgl';
+import CartoProvider from '@vizzuality/layer-manager-provider-carto';
+import { LayerManager, Layer } from '@vizzuality/layer-manager-react';
+
+import { useLayers } from 'hooks/useLayers';
 
 import Map from 'components/map';
 import Controls from 'components/map/controls';
@@ -16,16 +19,26 @@ import { ProjectMapParams } from 'types/project';
 import { useProjectsMap } from 'services/projects/projectService';
 
 import LocationSearcher from './location-searcher';
+import MapLayersSelector from './map-layers-selector';
 import MapPin from './pin';
 import MapPinCluster from './pin-cluster';
 import { DiscoverMapProps } from './types';
 
-export const DiscoverMap: FC<DiscoverMapProps> = () => {
-  const [viewport, setViewport] = useState({});
+const cartoProvider = new CartoProvider();
 
+export const DiscoverMap: FC<DiscoverMapProps> = () => {
   const { bbox } = useSelector((state) => state['/projects']);
 
+  const [viewport, setViewport] = useState({});
   const [bounds, setBounds] = useState(null);
+  const [visibleLayers, setVisibleLayers] = useState<string[]>([]);
+  const { layers } = useLayers();
+  const { query } = useRouter();
+  const { projectsMap } = useProjectsMap(query as ProjectMapParams);
+
+  const handleViewportChange = useCallback((vw) => {
+    setViewport(vw);
+  }, []);
 
   useEffect(() => {
     setBounds({
@@ -35,21 +48,24 @@ export const DiscoverMap: FC<DiscoverMapProps> = () => {
     });
   }, [bbox]);
 
-  const { query } = useRouter();
-
-  const { projectsMap } = useProjectsMap(query as ProjectMapParams);
-
-  const handleViewportChange = useCallback((vw) => {
-    setViewport(vw);
-  }, []);
-
   return (
     <>
       <div className="relative w-full h-full">
         <Map bounds={bounds} viewport={viewport} onMapViewportChange={handleViewportChange}>
           {(map) => (
             <>
-              <LayerManager map={map} plugin={PluginMapboxGl}></LayerManager>
+              <LayerManager
+                map={map}
+                plugin={MapboxGLPlugin}
+                providers={{
+                  [cartoProvider.name]: cartoProvider.handleData,
+                }}
+              >
+                {layers.map(({ specification: layerSpec }) => {
+                  if (!layerSpec || !visibleLayers.includes(layerSpec.id)) return null;
+                  return <Layer key={layerSpec.id} {...layerSpec} opacity={0.7} />;
+                })}
+              </LayerManager>
 
               <ClusterLayer
                 data={projectsMap}
@@ -61,11 +77,14 @@ export const DiscoverMap: FC<DiscoverMapProps> = () => {
           )}
         </Map>
 
-        <Controls className="absolute bottom-10 xl:bottom-6 right-11">
+        <div className="absolute flex gap-2 top-3.5 left-3.5 text-gray-800 text-sm">
+          <MapLayersSelector onActiveLayersChange={setVisibleLayers} />
+          <LocationSearcher />
+        </div>
+
+        <Controls className="absolute border bottom-10 xl:bottom-6 right-11">
           <ProjectLegend />
         </Controls>
-
-        <LocationSearcher />
       </div>
     </>
   );
