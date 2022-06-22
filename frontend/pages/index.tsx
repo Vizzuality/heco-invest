f985ddee3c3a12faf8549809a945573624473e14
anamontiaga <amontiaga@gmail.com>
2022-06-22 21:37:14 +0200

    homepage: change icons

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -20,7 +20,7 @@ import { StaticPageLayoutProps } from 'layouts/static-page';
 import { PageComponent } from 'types';
 
 import ConnectIcon from 'svgs/home/connect.svg';
-import ReportBackIcon from 'svgs/home/report-back.svg';
+import CreateAccountIcon from 'svgs/home/create-account.svg';
 import SearchFindIcon from 'svgs/home/search-find.svg';
 
 export const getStaticProps = withLocalizedRequests(async ({ locale }) => {
@@ -250,14 +250,29 @@ const Home: PageComponent<HomePageProps, StaticPageLayoutProps> = () => {
           </h2>
           <p className="max-w-md mx-auto mt-4 text-base text-center md:mt-8 sm:max-w-xl md:max-w-4xl sm:text-lg md:text-xl">
             <FormattedMessage
-              defaultMessage="Reach out for what you are looking for, from either <span>Investors</span> or <span>Project Developers</span> and <span>start the conversation</span>. Make sure to update us to help us track your contribution to preserving the Amazon: <span>the future is in your hands too!</span>"
-              id="Y7/cBd"
+              defaultMessage="Reach out for what you are looking for, from either <span>Investors</span> or <span>Project Developers</span> and start the conversation."
+              id="Wm7zf8"
               values={{
                 span: (chunks) => <span className="font-semibold">{chunks}</span>,
               }}
             />
           </p>
           <div className="grid grid-rows-3 gap-6 mt-12 md:mt-20 md:grid-rows-none md:grid-cols-3">
+            <div className="px-6 py-6 text-center md:py-10 bg-green-light/20 rounded-4xl">
+              <Icon
+                icon={CreateAccountIcon}
+                className="w-24 h-24 p-3 mx-auto bg-white rounded-full text-green-dark"
+              />
+              <h3 className="mt-4 text-xl font-medium uppercase md:mt-14">
+                <FormattedMessage defaultMessage="Create an account" id="0vL5u1" />
+              </h3>
+              <p className="mt-2 md:mt-2.5 text-black/70">
+                <FormattedMessage
+                  defaultMessage="Inspire and educate other users by coming back to the platform and updating us on your progress. We’ll be able to track the Amazon’s improvement thanks to your contribution!"
+                  id="O1mdXY"
+                />
+              </p>
+            </div>
             <div className="px-6 py-6 text-center md:py-10 bg-green-light/20 rounded-4xl">
               <Icon
                 icon={SearchFindIcon}
@@ -285,21 +300,6 @@ const Home: PageComponent<HomePageProps, StaticPageLayoutProps> = () => {
                 <FormattedMessage
                   defaultMessage="Start connecting with people to create impact. You can find investors, opportunities to invest in and much much more..."
                   id="xusigZ"
-                />
-              </p>
-            </div>
-            <div className="px-6 py-6 text-center md:py-10 bg-green-light/20 rounded-4xl">
-              <Icon
-                icon={ReportBackIcon}
-                className="w-24 h-24 p-3 mx-auto bg-white rounded-full text-green-dark"
-              />
-              <h3 className="mt-4 text-xl font-medium uppercase md:mt-14">
-                <FormattedMessage defaultMessage="Report back" id="RczAkW" />
-              </h3>
-              <p className="mt-2 md:mt-2.5 text-black/70">
-                <FormattedMessage
-                  defaultMessage="Inspire and educate other users by coming back to the platform and updating us on your progress. We’ll be able to track the Amazon’s improvement thanks to your contribution!"
-                  id="O1mdXY"
                 />
               </p>
             </div>
