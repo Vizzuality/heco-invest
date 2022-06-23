f6f2d318053fdcbd6bfc5a84dff1e257fd6b6584
anamontiaga <amontiaga@gmail.com>
2022-06-23 18:57:27 +0200

    homepage use platform section: responsive mobile

--- frontend/containers/home/hero/component.tsx
+++ frontend/containers/home/hero/component.tsx
@@ -4,14 +4,14 @@ import LayoutContainer from 'components/layout-container';
 
 export const Hero = () => (
   <div className="-mt-28 lg:-mt-44 pt-24 sm:pt-40 md:pt-56 bg-cover bg-center bg-green-dark bg-[url('/images/home-hero.jpg')]">
-    <div className="text-center">
-      <h1 className="max-w-md mx-auto font-serif text-3xl font-bold sm:max-w-xl md:max-w-5xl md:text-6xl text-green-light">
+    <div className="px-4 sm:text-center sm:px-6 lg:px-8">
+      <h1 className="font-serif text-3xl font-bold sm:mx-auto sm:max-w-xl md:max-w-5xl md:text-6xl text-green-light">
         <FormattedMessage
           defaultMessage="Be part of the biggest change in the Colombian Amazon"
           id="ZiErTG"
         />
       </h1>
-      <p className="max-w-md mx-auto mt-2 text-base text-white md:max-w-2xl sm:text-lg md:text-xl">
+      <p className="mt-2 text-base text-white sm:mx-auto md:max-w-2xl sm:text-lg md:text-xl">
         <FormattedMessage
           defaultMessage="Connecting investors, donors and philanthropists with carefully identified investment opportunities."
           id="8i6S4q"
