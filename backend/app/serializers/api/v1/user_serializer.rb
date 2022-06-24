d397d184a1b69c142053299390c7b18f1dd0de3c
martintomas <martintomas.it@gmail.com>
2022-06-24 11:09:25 +0200

    refactoring: Adding created_at attribute to all serializers

--- backend/app/serializers/api/v1/user_serializer.rb
+++ backend/app/serializers/api/v1/user_serializer.rb
@@ -1,7 +1,7 @@
 module API
   module V1
     class UserSerializer < BaseSerializer
-      attributes :first_name, :last_name, :email, :role
+      attributes :first_name, :last_name, :email, :role, :created_at
       attribute :confirmed, &:confirmed?
       attribute :approved, &:approved?
     end
