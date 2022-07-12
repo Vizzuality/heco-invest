8a1686dd0d7ce77dcfa0586f63ac331a2b6be64a
Tiago Garcia <tiagojsag@gmail.com>
2022-07-12 19:03:58 +0200

    Enable maps backend api

--- infrastructure/base/main.tf
+++ infrastructure/base/main.tf
@@ -7,6 +7,11 @@ terraform {
   }
 }
 
+resource "google_project_service" "geocoding_api" {
+  service            = "geocoding-backend.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "maps_api" {
   service            = "maps-backend.googleapis.com"
   disable_on_destroy = false
