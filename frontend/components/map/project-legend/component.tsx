d6a3aaf06a2a6fb9e06a4d5d191aaa30ca099534
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-07-12 16:43:14 +0100

    Implement collapsible map legend as per designs

--- frontend/components/map/project-legend/component.tsx
+++ frontend/components/map/project-legend/component.tsx
@@ -1,13 +1,16 @@
 import { FC, useCallback, useState } from 'react';
 
+import { ChevronDown as ChevronDownIcon } from 'react-feather';
 import { FormattedMessage } from 'react-intl';
 
 import cx from 'classnames';
 
 import { useId } from '@react-aria/utils';
+import { motion } from 'framer-motion';
 
 import { CategoryTagDot } from 'containers/category-tag';
 
+import Icon from 'components/icon';
 import { CategoryType } from 'types/category';
 
 import { useEnums } from 'services/enums/enumService';
@@ -23,28 +26,50 @@ export const ProjectLegend: FC<ProjectLegendProps> = ({ className }: ProjectLege
   const onToggleActive = useCallback(() => {
     setActive(!active);
   }, [active]);
+
   if (isLoading || isError) {
     return null;
   }
 
+  const legendVariants = {
+    open: { opacity: 1, height: 'auto' },
+    closed: { opacity: 0, height: 0 },
+  };
+
   return (
     <div
       className={cx({
-        'w-56 px-4 py-2 bg-white rounded-2xl shadow-xl text-xs flex flex-col': true,
+        'flex flex-col items-end h-full gap-px': true,
         [className]: !!className,
       })}
     >
       <button
         type="button"
-        aria-expanded={active}
         aria-controls={id}
-        className="relative flex flex-col items-start"
+        aria-expanded={active}
+        className="relative flex items-center justify-center w-8 h-8 bg-white rounded shadow-xl focus-visible:outline-green-dark"
         onClick={onToggleActive}
       >
-        <div className="font-semibold text-gray-700">
-          <FormattedMessage defaultMessage="Project categories" id="rginRA" />
-        </div>
-        {active && (
+        <Icon
+          aria-hidden={true}
+          icon={ChevronDownIcon}
+          className={cx({
+            'w-4.5 h-4.5 text-gray-600 shrink-0 transition duration-500': true,
+            '-scale-y-100': !active,
+          })}
+        />
+      </button>
+      <motion.div
+        id={id}
+        animate={active ? 'open' : 'closed'}
+        transition={{ duration: 0.6 }}
+        variants={legendVariants}
+        className="w-56 z-10 bg-white rounded shadow-xl text-xs flex flex-col lg:min-w-[280px]"
+      >
+        <div className="px-2 py-2">
+          <div className="font-semibold text-black">
+            <FormattedMessage defaultMessage="Project categories" id="rginRA" />
+          </div>
           <ol className="flex flex-col gap-2 mt-2 text-left">
             {data.category.map((category) => (
               <li key={category.id}>
@@ -53,8 +78,8 @@ export const ProjectLegend: FC<ProjectLegendProps> = ({ className }: ProjectLege
               </li>
             ))}
           </ol>
-        )}
-      </button>
+        </div>
+      </motion.div>
     </div>
   );
 };
