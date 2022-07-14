0ac67a5995c9820d7c3f7826f0bbe6d0db8dab8e
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-07-14 12:16:13 +0100

    Fix accessibility on ContentLanguageAlert component

--- frontend/containers/forms/content-language-alert/component.tsx
+++ frontend/containers/forms/content-language-alert/component.tsx
@@ -1,8 +1,10 @@
 import { FC, useState } from 'react';
 
 import { X as XIcon } from 'react-feather';
+import { FormattedMessage } from 'react-intl';
 
 import Alert from 'components/alert';
+import Button from 'components/button';
 import Icon from 'components/icon';
 
 import type { ContentLanguageAlertProps } from './types';
@@ -19,11 +21,17 @@ export const ContentLanguageAlert: FC<ContentLanguageAlertProps> = ({
     <Alert className={className}>
       <div className="flex items-center w-full">
         <span className="flex-1">{children}</span>
-        <Icon
-          className="w-5 h-5 ml-2 cursor-pointer"
-          icon={XIcon}
+        <Button
+          className="ml-2 focus-visible:outline-green-dark"
+          theme="naked"
+          size="smallest"
           onClick={() => setIsOpen(false)}
-        />
+        >
+          <span className="sr-only">
+            <FormattedMessage defaultMessage="Dismiss" id="TDaF6J" />
+          </span>
+          <Icon className="w-5 h-5" icon={XIcon} />
+        </Button>
       </div>
     </Alert>
   );
