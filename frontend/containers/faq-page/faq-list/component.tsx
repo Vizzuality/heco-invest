e223b97113e9f20dd77d9c6813555666057c1ff0
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-07-29 13:07:22 +0100

    Add FaqList, ListItem & ItemValidationTypes to help build consistent FAQ lists

---
+++ frontend/containers/faq-page/faq-list/component.tsx
@@ -0,0 +1,79 @@
+import { FC } from 'react';
+
+import { FormattedMessage } from 'react-intl';
+
+import cx from 'classnames';
+
+import { FaqListProps, ListItemProps, ItemValidationTypes } from './types';
+
+export const FaqList: FC<FaqListProps> = ({ className, level = 'one', children }: FaqListProps) => {
+  return (
+    <ol
+      className={cx({
+        'ml-6': true,
+        'my-4 font-medium list-decimal': level === 'one',
+        'font-normal list-lower-latin': level === 'two',
+        'text-sm list-lower-roman': level === 'three',
+        [className]: !!className,
+      })}
+    >
+      {children}
+    </ol>
+  );
+};
+
+export const ListItem: FC<ListItemProps> = ({
+  className,
+  title,
+  description,
+  inlineDescription = false,
+  mandatory = false,
+  validationType,
+  children,
+}: ListItemProps) => {
+  return (
+    <li
+      className={cx({
+        [className]: !!className,
+      })}
+    >
+      {title}
+      {inlineDescription && ': '}
+      {validationType && (
+        <>
+          {' '}
+          <span className="italic font-normal">
+            (
+            {validationType === ItemValidationTypes.Max600Chars && (
+              <FormattedMessage defaultMessage="max 600 characters" id="UhT/Dv" />
+            )}
+            {validationType === ItemValidationTypes.SelectMultiple && (
+              <FormattedMessage defaultMessage="select multiple" id="rsWAqe" />
+            )}
+            {validationType === ItemValidationTypes.SelectOne && (
+              <FormattedMessage defaultMessage="select one" id="gwLlk9" />
+            )}
+            {validationType === ItemValidationTypes.YesNo && (
+              <FormattedMessage defaultMessage="Yes / No" id="aNY6/x" />
+            )}
+            )
+          </span>
+        </>
+      )}
+      {mandatory && ' *'}
+      {description && (
+        <p
+          className={cx({
+            'font-light text-gray-600': true,
+            inline: inlineDescription,
+          })}
+        >
+          {description}
+        </p>
+      )}
+      {children}
+    </li>
+  );
+};
+
+export default FaqList;
---
+++ frontend/containers/faq-page/faq-list/types.ts
@@ -0,0 +1,30 @@
+import { PropsWithChildren } from 'react';
+
+export enum ItemValidationTypes {
+  YesNo = 'yes-no',
+  SelectOne = 'select-one',
+  SelectMultiple = 'select-multiple',
+  Max600Chars = 'max-600-caracters',
+}
+
+export type FaqListProps = PropsWithChildren<{
+  /** ClassNames to apply to the container */
+  className?: string;
+  /** List nesting level. Defaults to `one` */
+  level?: 'one' | 'two' | 'three';
+}>;
+
+export type ListItemProps = PropsWithChildren<{
+  /** ClassNames to apply to the container */
+  className?: string;
+  /** Title of the list item */
+  title: string;
+  /** Description of the list item */
+  description?: string;
+  /** Whether the description should be displayed inline with the title. Defaults to `false` */
+  inlineDescription?: boolean;
+  /** Validation type. Defaults to `none` */
+  validationType?: ItemValidationTypes;
+  /** Whether it is mandatory field. Defaults to `false` */
+  mandatory?: boolean;
+}>;
