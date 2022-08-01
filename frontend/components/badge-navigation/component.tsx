0965b96f84d55bdfd14d08e88451c9e4aa4e3fae
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-08-01 15:18:01 +0100

    Badge navigation component not supports badge position, vertical layout, and simple theme

--- frontend/components/badge-navigation/component.tsx
+++ frontend/components/badge-navigation/component.tsx
@@ -12,16 +12,45 @@ import { BadgeNavigationProps } from './types';
 
 export const BadgeNavigation: FC<BadgeNavigationProps> = ({
   className,
+  theme = 'default',
+  orientation = 'horizontal',
+  badgePosition = 'right',
   activeId,
   items,
 }: BadgeNavigationProps) => {
   const intl = useIntl();
 
+  const badgeElement = (number: number, isActive: boolean) => {
+    if (!number) return null;
+
+    return (
+      <span
+        className={cx({
+          'flex items-center justify-center w-6 h-6 px-1 border text-xs font-semibold text-black rounded-full min-w-min border-beige':
+            true,
+          'bg-background-dark': theme !== 'simple',
+          'bg-white': isActive && theme === 'simple',
+          'ml-2': badgePosition === 'right',
+          'mr-2': badgePosition === 'left',
+        })}
+      >
+        {number}
+      </span>
+    );
+  };
+
   return (
     <div className={className}>
       <nav className="relative mx-2">
-        <span className="absolute left-0 right-0 border rounded-full top-2 bottom-2 border-beige" />
-        <ol className="flex gap-2 py-2 whitespace-nowrap">
+        {orientation === 'horizontal' && theme !== 'simple' && (
+          <span className="absolute left-0 right-0 border rounded-full top-2 bottom-2 border-beige" />
+        )}
+        <ol
+          className={cx({
+            'flex gap-2 py-2 whitespace-nowrap': true,
+            'flex-col': orientation === 'vertical',
+          })}
+        >
           {items.map(({ id, name, link, number }) => {
             const isActive = id === activeId;
 
@@ -29,22 +58,21 @@ export const BadgeNavigation: FC<BadgeNavigationProps> = ({
               <li key={link} className="transition-all">
                 <Link href={link}>
                   <a
-                    className="flex rounded-full focus-visible:outline focus-visible:outline-green-dark focus-visible:outline-2 focus-visible:outline-offset-2"
+                    className="inline-flex rounded-full focus-visible:outline focus-visible:outline-green-dark focus-visible:outline-2 focus-visible:outline-offset-2"
                     aria-current={isActive ? 'location' : false}
                   >
                     <Tag
                       className={cx({
                         'text-sm hover:font-medium hover:text-black': true,
-                        'bg-white font-semibold text-black shadow-sm': isActive,
-                        'text-green-dark border-none p-px': !isActive,
+                        'font-semibold text-black': isActive,
+                        'bg-white shadow-sm': isActive && theme !== 'simple',
+                        'text-green-dark p-px': !isActive,
+                        'border-none': !isActive || theme === 'simple',
                       })}
                     >
+                      {badgePosition === 'left' && badgeElement(number, isActive)}
                       {name}
-                      {number !== undefined && (
-                        <span className="flex items-center justify-center w-6 h-6 px-1 ml-2 text-xs font-semibold text-black border rounded-full min-w-min bg-background-dark border-beige">
-                          {number}
-                        </span>
-                      )}
+                      {badgePosition === 'right' && badgeElement(number, isActive)}
                     </Tag>
                   </a>
                 </Link>
