9e66e45698d6050e93700bfdae1f796ff0e32b5c
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-08-02 16:17:50 +0100

    Improve Dashboard layout loading displays

--- frontend/layouts/dashboard/component.tsx
+++ frontend/layouts/dashboard/component.tsx
@@ -1,5 +1,7 @@
 import { FC, useRef } from 'react';
 
+import cx from 'classnames';
+
 import LayoutContainer from 'components/layout-container';
 import Loading from 'components/loading';
 import { UserRoles } from 'enums';
@@ -48,15 +50,23 @@ export const DashboardLayout: FC<DashboardLayoutProps> = ({
               </div>
             </LayoutContainer>
           </div>
-          <div ref={mainContainerRef} className="h-full overflow-y-auto bg-background-dark">
-            <LayoutContainer className="flex flex-col gap-20 py-8 md:flex-row">
+          <div
+            ref={mainContainerRef}
+            className="relative h-full overflow-y-auto bg-background-dark"
+          >
+            <LayoutContainer
+              className={cx({
+                'relative flex flex-col gap-20 py-8 md:flex-row': true,
+                'h-full': isLoading,
+              })}
+            >
               {sidebar && (
                 <aside className="relative w-3/12">
                   <div className="md:sticky md:top-8">{sidebar}</div>
                 </aside>
               )}
-              <main className="flex-1 h-full">
-                {children}
+              <main className="relative flex-1 h-full">
+                <div className={cx({ hidden: isLoading })}>{children}</div>
                 {isLoading && (
                   <div className="absolute flex items-center justify-center bg-background-dark top-px bottom-px left-px right-px rounded-2xl backdrop-blur-sm">
                     <Loading visible={true} iconClassName="w-10 h-10" />
