3f98e4c96a0701e1c63f6a5af25c97407507bcfc
barbara-chaves <barbarabarcellos90@gmail.com>
2022-08-04 15:36:28 +0200

    Change email message

--- frontend/containers/share-icons/component.tsx
+++ frontend/containers/share-icons/component.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react';
 
 import { Facebook, Link, Mail, Twitter } from 'react-feather';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import classNames from 'classnames';
 
@@ -15,16 +15,38 @@ import { ShareIconsProps } from '.';
 
 export const ShareIcons: FC<ShareIconsProps> = ({ title }) => {
   const [copied, setCopied] = useState(false);
+  const { formatMessage } = useIntl();
 
   const { asPath } = useRouter();
-  const shareLinkUrl = `https://staging.hecoinvest.org${asPath}`;
-  // const shareLinkUrl = `${process.env.NEXT_PUBLIC_FRONTEND_URL}${asPath}`;
+  // Facebook don't support locales, so we use the staging url when in local
+  const shareLinkUrl = process.env.NEXT_PUBLIC_PROXY_BACKEND
+    ? `https://staging.hecoinvest.org${asPath}`
+    : `${process.env.NEXT_PUBLIC_FRONTEND_URL}${asPath}`;
+
   const isProjectPage = asPath.includes('/project/');
 
   const shareLinks = {
     facebook: `https://www.facebook.com/sharer/sharer.php?u=${shareLinkUrl}`,
     twitter: `https://twitter.com/intent/tweet?url=${shareLinkUrl}`,
-    email: `mailto:?subject=${title}&body=${shareLinkUrl}`,
+    email: `mailto:?subject=${formatMessage(
+      {
+        defaultMessage: 'Someone has shared a HeCo invest {shared} with you',
+        id: 'wD2sM8',
+      },
+      { shared: isProjectPage ? 'project' : 'contact' }
+    )}&body=${formatMessage(
+      {
+        defaultMessage: 'Reach out {title} on the HeCo Invest through the link',
+        id: '1AM5V6',
+      },
+      {
+        title,
+      }
+    )} ${shareLinkUrl}%0D%0A%0D%0A${formatMessage({
+      defaultMessage:
+        'HeCo Invest is a digital collaborative platform aimed to support filling the conservation financing gap in the Amazon Basin by optimizing project financing channels in this region.',
+      id: 'Lw4PWP',
+    })}`,
   };
 
   const copyToCkipboard = () => {
@@ -41,14 +63,14 @@ export const ShareIcons: FC<ShareIconsProps> = ({ title }) => {
       })}
     >
       <div className="flex items-center gap-2">
-        <span className="text-sm leading-6 text-gray-600 mr-2">
+        <span className="mr-2 text-sm leading-6 text-gray-600">
           <FormattedMessage defaultMessage="Share" id="OKhRC6" />
         </span>
         <Button
           to={shareLinks.twitter}
           target="_blank"
           rel="noopener noreferrer"
-          className="py-0 px-0 w-8 h-8 flex transition-opacity items-center justify-center bg-beige rounded-full text-beige hover:opacity-60 hover:text-beige"
+          className="flex items-center justify-center w-8 h-8 px-0 py-0 transition-opacity rounded-full bg-beige text-beige hover:opacity-60 hover:text-beige"
         >
           <Icon fill="white" icon={Twitter} />
         </Button>
@@ -56,25 +78,25 @@ export const ShareIcons: FC<ShareIconsProps> = ({ title }) => {
           to={shareLinks.facebook}
           target="_blank"
           rel="noopener noreferrer"
-          className="py-0 px-0 w-8 h-8 flex transition-opacity items-center justify-center bg-beige rounded-full text-transparent hover:opacity-60 hover:text-transparent"
+          className="flex items-center justify-center w-8 h-8 px-0 py-0 text-transparent transition-opacity rounded-full bg-beige hover:opacity-60 hover:text-transparent"
         >
           <Icon fill="white" icon={Facebook} />
         </Button>
         <Button
           to={shareLinks.email}
-          className="py-0 px-0 w-8 h-8 flex transition-opacity items-center justify-center bg-beige text-beige rounded-full hover:opacity-60 hover:text-beige"
+          className="flex items-center justify-center w-8 h-8 px-0 py-0 transition-opacity rounded-full bg-beige text-beige hover:opacity-60 hover:text-beige"
         >
           <Icon icon={Mail} fill="white" />
         </Button>
         <Button
           onClick={copyToCkipboard}
-          className="py-0 px-0 w-8 h-8 flex transition-opacity items-center justify-center bg-beige text-white rounded-full hover:opacity-60 hover:text-white"
+          className="flex items-center justify-center w-8 h-8 px-0 py-0 text-white transition-opacity rounded-full bg-beige hover:opacity-60 hover:text-white"
         >
           <Icon icon={Link} />
         </Button>
       </div>
       {copied && (
-        <div className="lg:absolute translate-y-4 -translate-x-10 min-w-fit">
+        <div className="-translate-x-10 translate-y-4 lg:absolute min-w-fit">
           <Toast
             id="copied-success"
             level="success"
