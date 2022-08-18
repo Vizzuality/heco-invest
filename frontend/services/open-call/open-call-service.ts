005ba80ecb2b58e50d6f1c393a8d7050da705ab0
barbara-chaves <barbarabarcellos90@gmail.com>
2022-08-18 16:15:46 +0200

    Change get open call by id

--- frontend/services/open-call/open-call-service.ts
+++ frontend/services/open-call/open-call-service.ts
@@ -9,8 +9,7 @@ import { useLocalizedQuery } from 'hooks/query';
 import { Languages, Queries } from 'enums';
 import { OpenCall, OpenCallDto } from 'types/open-calls';
 
-import API, { RawApi } from 'services/api';
-import { getInvestor } from 'services/investors/investorsService';
+import API from 'services/api';
 import { ErrorResponse, ResponseData } from 'services/types';
 
 export const useCreateOpenCall = (): UseMutationResult<
@@ -26,43 +25,12 @@ export const useCreateOpenCall = (): UseMutationResult<
 };
 
 export const getOpenCall = async (id: string): Promise<OpenCall> => {
-  const response = await RawApi.get(`/api/v1/open_calls/${id}`);
-  const {
-    investor: {
-      data: { id: investorId },
+  const response = await API.get<ResponseData<OpenCall>>(`/api/v1/open_calls/${id}`, {
+    params: {
+      includes: 'investor,country,department,municipality',
     },
-    country: {
-      data: { id: countryId },
-    },
-    municipality: {
-      data: { id: municipalityId },
-    },
-    department: {
-      data: { id: departmentId },
-    },
-  } = response.data.data.relationships;
-  const investor = await getInvestor(investorId);
-  const country = await API.get(`/api/v1/locations/${countryId}`);
-  const department = await API.get(`/api/v1/locations/${departmentId}`);
-  const municipality = await API.get(`/api/v1/locations/${municipalityId}`);
-
-  const openCall = {
-    id: response.data.data.id,
-    ...response.data.data.attributes,
-    investor,
-    country: country?.data?.data,
-    department: department?.data?.data,
-    municipality: municipality?.data?.data,
-  };
-
-  return openCall;
-  // Use the code bellow when the BE team add the 'includes' param to the open call endpoint
-  // const response = await API.get<ResponseData<OpenCall>>(`/api/v1/open_calls/${id}`, {
-  //   params: {
-  //     includes: 'investor,country,department,municipality',
-  //   },
-  // });
-  // return response.data.data;
+  });
+  return response.data.data;
 };
 
 export const useOpenCall = (
