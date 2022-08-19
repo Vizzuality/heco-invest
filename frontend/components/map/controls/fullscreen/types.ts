887d9692beecaa90edd342dbbf390d7a6dd305c1
Clément Prod'homme <contact@clementprodhomme.fr>
2022-08-19 16:10:36 +0200

    Fix type issue

--- frontend/components/map/controls/fullscreen/types.ts
+++ frontend/components/map/controls/fullscreen/types.ts
@@ -2,7 +2,7 @@ import { MutableRefObject } from 'react';
 
 export interface FullscreenControlProps {
   /** Reference ot the map's container */
-  mapRef: MutableRefObject<HTMLElement>;
+  mapRef: MutableRefObject<any>;
   /** Classname to apply to the control */
   className?: string;
 }
