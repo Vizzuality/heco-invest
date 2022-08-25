97db37a154bbf6b9f18a57ab48daa4c237a2c3bd
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-08-25 16:37:57 +0100

    Add OpenCallApplicationModal to the open call public page (header + cta at the bottom)

--- frontend/containers/open-call-page/investor/types.ts
+++ frontend/containers/open-call-page/investor/types.ts
@@ -3,6 +3,4 @@ import { OpenCall } from 'types/open-calls';
 export type OpenCallInvestorProps = {
   /** The open call object we are displaying the banner for */
   openCall: OpenCall;
-  /** Function to apply to a open call */
-  handleApply: () => void;
 };
