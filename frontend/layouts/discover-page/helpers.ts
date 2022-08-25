22f684ae59cf3c3d2a95af2f032adb15b46261fa
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-08-25 14:20:28 +0100

    Add the ability to specify the target sorting options to the useSortingByOptions hook

--- frontend/layouts/discover-page/helpers.ts
+++ frontend/layouts/discover-page/helpers.ts
@@ -1,32 +1,50 @@
 import { useIntl } from 'react-intl';
 
+import { compact } from 'lodash-es';
+
 import { SortingOptionType } from 'components/sorting-buttons';
 
 /** Returns the sorting-by types array with translated labels  */
-export const useSortingByOptions = (): SortingOptionType[] => {
+export const useSortingByOptions = (target?: 'projects' | 'openCalls'): SortingOptionType[] => {
   const { formatMessage } = useIntl();
-  return [
-    { key: 'name', label: formatMessage({ defaultMessage: 'Name', id: 'HAlOn1' }) },
-    { key: 'created_at', label: formatMessage({ defaultMessage: 'Date', id: 'P7PLVj' }) },
-    {
-      key: 'total_impact',
-      label: formatMessage({ defaultMessage: 'Impact score', id: '2GBpne' }),
-    },
-    {
-      key: 'biodiversity_impact',
-      label: formatMessage({ defaultMessage: 'Biodiversity impact', id: 'BvO1iK' }),
-    },
-    {
-      key: 'climate_impact',
-      label: formatMessage({ defaultMessage: 'Climate impact', id: 'TUFmRM' }),
-    },
-    {
-      key: 'water_impact',
-      label: formatMessage({ defaultMessage: 'Water impact', id: 'W5x0GV' }),
-    },
-    {
-      key: 'community_impact',
-      label: formatMessage({ defaultMessage: 'Community impact', id: 'PGlFh/' }),
-    },
-  ];
+
+  const sortingOptions = {
+    general: [
+      { key: 'name', label: formatMessage({ defaultMessage: 'Name', id: 'HAlOn1' }) },
+      { key: 'created_at', label: formatMessage({ defaultMessage: 'Date', id: 'P7PLVj' }) },
+    ],
+    projects: [
+      {
+        key: 'total_impact',
+        label: formatMessage({ defaultMessage: 'Impact score', id: '2GBpne' }),
+      },
+      {
+        key: 'biodiversity_impact',
+        label: formatMessage({ defaultMessage: 'Biodiversity impact', id: 'BvO1iK' }),
+      },
+      {
+        key: 'climate_impact',
+        label: formatMessage({ defaultMessage: 'Climate impact', id: 'TUFmRM' }),
+      },
+      {
+        key: 'water_impact',
+        label: formatMessage({ defaultMessage: 'Water impact', id: 'W5x0GV' }),
+      },
+      {
+        key: 'community_impact',
+        label: formatMessage({ defaultMessage: 'Community impact', id: 'PGlFh/' }),
+      },
+    ],
+    openCalls: [
+      {
+        key: 'instrument_type',
+        label: formatMessage({ defaultMessage: 'Instrument type', id: 'fDd10o' }),
+      },
+    ],
+  };
+
+  return compact([
+    ...sortingOptions['general'],
+    ...(sortingOptions[target] || []),
+  ]) as SortingOptionType[];
 };
