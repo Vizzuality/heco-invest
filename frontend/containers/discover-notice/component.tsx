479365ac0bb4c97103fb5f0e56c07be88441627d
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-08-25 16:49:20 +0100

    Add notice to the discover/search/open-calls page

---
+++ frontend/containers/discover-notice/component.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+
+import { X as XIcon } from 'react-feather';
+
+import Alert from 'components/alert';
+import Button from 'components/button';
+
+import { DiscoverNoticeProps } from './types';
+
+export const DiscoverNotice: FC<DiscoverNoticeProps> = ({
+  className,
+  isVisible,
+  children,
+  onClose,
+}) => {
+  if (!isVisible) return null;
+  return (
+    <div>
+      <Alert className={className}>
+        <div className="flex items-center">
+          <div className="text-base">{children}</div>
+          <div className="flex items-center justify-center">
+            <Button
+              className="ml-2 focus-visible:outline-green-dark"
+              theme="naked"
+              size="smallest"
+              onClick={onClose}
+            >
+              <XIcon className="w-5 h-5" />
+            </Button>
+          </div>
+        </div>
+      </Alert>
+    </div>
+  );
+};
+
+export default DiscoverNotice;
--- frontend/pages/discover/open-calls.tsx
+++ frontend/pages/discover/open-calls.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
@@ -11,6 +11,7 @@ import { usePagination } from 'hooks/usePagination';
 
 import { loadI18nMessages } from 'helpers/i18n';
 
+import DiscoverNotice from 'containers/discover-notice';
 import OpenCallCard from 'containers/open-call-card';
 
 import Loading from 'components/loading';
@@ -40,39 +41,58 @@ const OpenCallsPage: PageComponent<OpenCallsPageProps, DiscoverPageLayoutProps> 
 }) => {
   const openCallsContainerRef = useRef(null);
   const { props: paginationProps } = usePagination(meta);
+  const [showNotice, setShowNotice] = useState<boolean>(false);
 
   useScrollOnQuery({ ref: openCallsContainerRef });
 
   const hasOpenCalls = openCalls?.length > 0 || false;
 
+  useEffect(() => {
+    const isClosed = localStorage.getItem('discover-open-calls-notice-closed');
+    setShowNotice(!isClosed);
+  }, []);
+
+  const handleCloseNoticeClick = () => {
+    localStorage.setItem('discover-open-calls-notice-closed', 'true');
+    setShowNotice(false);
+  };
+
   return (
-    <div className="flex flex-col w-full h-full pb-2 lg:p-1 lg:-m-1 lg:gap-0 lg:overflow-hidden lg:flex-row">
-      <div className="relative flex flex-col w-full lg:overflow-hidden ">
-        <div
-          ref={openCallsContainerRef}
-          className={cx({
-            'relative flex-grow lg:pr-2.5': true,
-            'lg:overflow-y-auto': !loading,
-            'lg:pointer-events-none lg:overflow-hidden': loading,
-          })}
-        >
-          {loading && (
-            <span className="absolute bottom-0 z-20 flex items-center justify-center bg-gray-600 bg-opacity-20 top-1 left-1 right-3 rounded-2xl">
-              <Loading visible={loading} iconClassName="w-10 h-10" />
-            </span>
-          )}
-          <div className="grid grid-cols-1 gap-6 p-1 md:grid-cols-2">
-            {openCalls.map((openCall) => (
-              <OpenCallCard key={openCall.id} openCall={openCall} />
-            ))}
-            {!loading && !hasOpenCalls && (
-              <FormattedMessage defaultMessage="No open calls" id="O3WloJ" />
+    <>
+      <div className="flex flex-col w-full h-full pb-2 lg:p-1 lg:-m-1 lg:gap-0 lg:overflow-hidden lg:flex-row">
+        <div className="relative flex flex-col w-full lg:overflow-hidden ">
+          <DiscoverNotice className="mb-1" isVisible={showNotice} onClose={handleCloseNoticeClick}>
+            <FormattedMessage
+              defaultMessage="Open calls are funding offers from investors requesting project developer proposals to grow their businesses. Note that these open proposals are available for a limited time and have unique application requirements."
+              id="Cf2YAW"
+            />
+          </DiscoverNotice>
+          <div
+            ref={openCallsContainerRef}
+            className={cx({
+              'relative flex-grow lg:pr-2.5': true,
+              'lg:overflow-y-auto': !loading,
+              'lg:pointer-events-none lg:overflow-hidden': loading,
+            })}
+          >
+            {loading && (
+              <span className="absolute bottom-0 z-20 flex items-center justify-center bg-gray-600 bg-opacity-20 top-1 left-1 right-3 rounded-2xl">
+                <Loading visible={loading} iconClassName="w-10 h-10" />
+              </span>
             )}
+            <div className="grid grid-cols-1 gap-6 p-1 md:grid-cols-2">
+              {openCalls.map((openCall) => (
+                <OpenCallCard key={openCall.id} openCall={openCall} />
+              ))}
+              {!loading && !hasOpenCalls && (
+                <FormattedMessage defaultMessage="No open calls" id="O3WloJ" />
+              )}
+            </div>
           </div>
+          {hasOpenCalls && <Pagination className="w-full pt-2 -mb-2" {...paginationProps} />}
         </div>
-        {hasOpenCalls && <Pagination className="w-full pt-2 -mb-2" {...paginationProps} />}
       </div>
-    </div>
+    </>
   );
 };
 
