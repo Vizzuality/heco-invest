c5a1157b39bcb4275aa832ad72bca42e26e572a3
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-09-06 10:47:54 +0100

    Remove incorrect (and not needed( useAccount include in the InviteUsersModal

--- frontend/containers/users/invite-users-modal/component.tsx
+++ frontend/containers/users/invite-users-modal/component.tsx
@@ -30,7 +30,7 @@ export const InviteUsersModal: FC<InviteUsersModalProps> = ({
 }: InviteUsersModalProps) => {
   const { formatMessage } = useIntl();
   const inviteUsers = useInviteUsers();
-  const { userAccount } = useAccount({ includes: 'owner' });
+  const { userAccount } = useAccount();
   const { name } = userAccount || {};
   const queryClient = useQueryClient();
 
