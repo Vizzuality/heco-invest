2e47dcf535a309929c2acd5a8d613d4d473e46c8
barbara-chaves <barbarabarcellos90@gmail.com>
2022-10-10 16:09:26 +0200

    Add _error page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,7 +1,6 @@
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 
 import { withLocalizedRequests } from 'hoc/locale';
 
@@ -12,6 +11,7 @@ import { loadI18nMessages } from 'helpers/i18n';
 import Button from 'components/button';
 import Head from 'components/head';
 import LayoutContainer from 'components/layout-container';
+import { Paths } from 'enums';
 import NakedLayout, { NakedLayoutProps } from 'layouts/naked';
 import Header from 'layouts/static-page/header';
 import { PageComponent } from 'types';
@@ -28,11 +28,10 @@ type InternalErrorProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 const InternalError: PageComponent<InternalErrorProps, NakedLayoutProps> = () => {
   const { formatMessage } = useIntl();
-  const { back } = useRouter();
 
   return (
     <div className="h-screen">
-      <Head title={formatMessage({ defaultMessage: 'Coming soon', id: 'e61Jf3' })} />
+      <Head title={formatMessage({ defaultMessage: 'Error 404', id: '1HohzV' })} />
       <Header />
       <LayoutContainer className="flex flex-col items-center justify-center h-full bg-background-light">
         <div className="mb-10 text-center">
@@ -40,7 +39,7 @@ const InternalError: PageComponent<InternalErrorProps, NakedLayoutProps> = () =>
             src="/images/not-found.png"
             height={307}
             width={814}
-            alt="500"
+            alt={formatMessage({ defaultMessage: 'Error 404', id: '1HohzV' })}
             objectFit="contain"
           />
           <h1 className="mt-10 mb-4 font-serif text-3xl text-green-dark">
@@ -48,12 +47,12 @@ const InternalError: PageComponent<InternalErrorProps, NakedLayoutProps> = () =>
           </h1>
           <p className="underline">
             <FormattedMessage
-              defaultMessage="It looks like the links is broken or the page has been removed."
-              id="5NPxff"
+              defaultMessage="It looks like the link is broken or the page has been removed."
+              id="oD3qdW"
             />
           </p>
         </div>
-        <Button onClick={() => back()}>
+        <Button to={Paths.Home}>
           <FormattedMessage defaultMessage="Go back" id="orvpWh" />
         </Button>
       </LayoutContainer>
