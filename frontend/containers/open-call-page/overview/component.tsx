56798c45ca145af0adb2a4e9741a265f6803ea77
Bárbara Chaves <barbarabarcellos90@gmail.com>
2022-10-14 14:52:54 +0200

    Let 1157 fe tweak the project and open call public page layout (#642)

--- frontend/containers/open-call-page/overview/component.tsx
+++ frontend/containers/open-call-page/overview/component.tsx
@@ -7,41 +7,26 @@ import LayoutContainer from 'components/layout-container';
 import { OpenCallOverviewTypes } from './types';
 
 export const Overview: FC<OpenCallOverviewTypes> = ({ openCall }) => {
-  const { country, municipality, department } = openCall;
+  const { country, department } = openCall;
 
   return (
-    <LayoutContainer id="overview" className="mt-10 mb-20 lg:mb-28">
-      <LayoutContainer className="font-serif lg:flex">
-        <div className="flex flex-col gap-10 lg:justify-between lg:flex-row">
-          <div className="flex flex-col space-y-4 lg:mb-12 lg:w-1/2">
-            <h2 className="text-2xl lg:text-3xl">
-              <FormattedMessage defaultMessage="Location" id="rvirM2" />
-            </h2>
-            <div className="flex flex-col space-y-1">
-              <div className="flex space-x-2 font-sans text-base">
-                <h3 className="font-semibold min-w-[130px]">
-                  <FormattedMessage defaultMessage="Country" id="vONi+O" />
-                </h3>
-                <p>{country?.name}</p>
-              </div>
-              {!!department && (
-                <div className="flex space-x-2 font-sans text-base">
-                  <h3 className="font-semibold min-w-[130px]">
-                    <FormattedMessage defaultMessage="State" id="ku+mDU" />
-                  </h3>
-                  <p>{department?.name}</p>
-                </div>
-              )}{' '}
-              {!!municipality && (
-                <div className="flex space-x-2 font-sans text-base">
-                  <h3 className="font-semibold min-w-[130px]">
-                    <FormattedMessage defaultMessage="Municipality" id="9I1zvK" />
-                  </h3>
-                  <p>{municipality?.name}</p>
-                </div>
-              )}
-            </div>
+    <LayoutContainer id="overview" className="mt-8">
+      <LayoutContainer>
+        <div className="flex flex-col sm:flex-row gap-x-8 gap-y-2">
+          <div className="flex gap-2 font-sans text-base">
+            <h3 className="font-semibold">
+              <FormattedMessage defaultMessage="Country" id="vONi+O" />
+            </h3>
+            <p>{country?.name}</p>
           </div>
+          {!!department && (
+            <div className="flex gap-2 font-sans text-base">
+              <h3 className="font-semibold">
+                <FormattedMessage defaultMessage="State" id="ku+mDU" />
+              </h3>
+              <p>{department?.name}</p>
+            </div>
+          )}
         </div>
       </LayoutContainer>
     </LayoutContainer>
--- frontend/containers/project-page/header/component.tsx
+++ frontend/containers/project-page/header/component.tsx
@@ -16,7 +16,6 @@ import ContactInformationModal from 'containers/social-contact/contact-informati
 import Button from 'components/button';
 import Icon from 'components/icon';
 import LayoutContainer from 'components/layout-container';
-import Tag from 'components/tag';
 import { CategoryType } from 'types/category';
 
 import { useAccount } from 'services/account';
@@ -110,7 +109,7 @@ export const Header: FC<HeaderProps> = ({ className, project }: HeaderProps) => 
               */}
               {category && (
                 <CategoryTag
-                  className="bg-white text-green-dark"
+                  className="text-sm bg-white text-green-dark"
                   category={category.id as CategoryType}
                 >
                   {category.name}
@@ -131,7 +130,7 @@ export const Header: FC<HeaderProps> = ({ className, project }: HeaderProps) => 
         </LayoutContainer>
       </div>
       <LayoutContainer className="flex flex-col justify-between mt-8 lg:flex-row">
-        <div className="w-full lg:w-6/12">
+        <div className="w-full lg:w-6/12 min-h-[160px] pr-2">
           {project.language && (
             <span className="block mb-4 text-sm text-gray-400">
               <FormattedMessage
@@ -146,38 +145,31 @@ export const Header: FC<HeaderProps> = ({ className, project }: HeaderProps) => 
           )}
           <p>{project.description}</p>
         </div>
-        <div className="lg:mr-4 p-6 bg-white drop-shadow-xl mb-16 lg:pb-8 lg:mb-[-70%] h-full lg:translate-y-[-70%] lg:max-w-4/12 rounded-2xl mt-8 lg:mt-0 flex flex-col">
+        <div className="lg:mr-4 p-6 bg-white drop-shadow-xl mb-16 lg:mb-[-70%] h-full lg:w-[395px] lg:translate-y-[-70%] lg:max-w-4/12 rounded-2xl mt-8 lg:mt-0 flex flex-col">
           {project.looking_for_funding ? (
-            <div className="flex flex-col gap-8 md:flex-row">
-              <div className="flex flex-col items-start justify-end w-full gap-2 text-center md:min-w-1/2">
-                <span id="ticket-size" className="text-2xl font-semibold">
+            <div className="flex flex-col gap-8 mb-8 md:flex-row">
+              <div className="flex flex-col items-start justify-end w-full gap-2 text-center sm:text-left md:min-w-1/2">
+                <span id="ticket-size" className="text-xl font-semibold">
                   {ticketSizeStr}
                 </span>
-                <span aria-labelledby="ticket-size" className="text-gray-400">
+                <span aria-labelledby="ticket-size" className="leading-4 text-gray-400">
                   <FormattedMessage defaultMessage="Ticket size" id="lfx6Nc" />
                 </span>
               </div>
               <div className="flex flex-col items-start justify-end w-full gap-2 text-left md:min-w-1/2">
-                <span
-                  id="instrument-types"
-                  className={cx({
-                    'font-semibold': true,
-                    'text-2xl': project.instrument_types.length <= 1,
-                    'text-xl': project.instrument_types.length > 1,
-                  })}
-                >
+                <span id="instrument-types" className="text-xl font-semibold">
                   {instrumentTypesStr}
                 </span>
                 <span
                   aria-labelledby="instrument-types"
-                  className="text-gray-400 whitespace-nowrap"
+                  className="leading-4 text-gray-400 whitespace-nowrap"
                 >
                   <FormattedMessage defaultMessage="Instrument type" id="fDd10o" />
                 </span>
               </div>
             </div>
           ) : (
-            <div className="max-w-sm text-gray-800">
+            <div className="max-w-sm mb-4 text-gray-800">
               <p className="text-lg font-semibold">
                 <FormattedMessage
                   defaultMessage="This project isn’t currently looking for funding."
@@ -193,7 +185,7 @@ export const Header: FC<HeaderProps> = ({ className, project }: HeaderProps) => 
             </div>
           )}
 
-          <div className="flex flex-col justify-between gap-4 mt-5 lg:flex-row">
+          <div className="flex flex-col justify-between gap-x-2 gap-y-4 lg:flex-row">
             <Button
               className="justify-center"
               theme="secondary-green"
@@ -209,7 +201,7 @@ export const Header: FC<HeaderProps> = ({ className, project }: HeaderProps) => 
               <FormattedMessage defaultMessage="Favorite" id="5Hzwqs" />
             </Button>
             <Button
-              className="w-full lg:max-w-[200px] justify-center"
+              className="justify-center w-full"
               disabled={!contacts.length}
               theme="primary-green"
               onClick={() => setIsContactInfoModalOpen(true)}
