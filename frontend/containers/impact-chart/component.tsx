51cec7464f604ae40a00bd593a06f46aca7d04bf
barbara-chaves <barbarabarcellos90@gmail.com>
2022-10-25 22:19:37 +0200

    change chart

--- frontend/containers/impact-chart/component.tsx
+++ frontend/containers/impact-chart/component.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useMemo, useRef } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
-import { Radar } from 'react-chartjs-2';
+import { PolarArea } from 'react-chartjs-2';
 import { useIntl } from 'react-intl';
 
 import classNames from 'classnames';
@@ -14,6 +14,7 @@ import {
   Tooltip as ChartTooltip,
   ChartData,
   ChartOptions,
+  ArcElement,
 } from 'chart.js';
 
 import FieldInfo from 'components/forms/field-info';
@@ -24,7 +25,39 @@ import { useEnums } from 'services/enums/enumService';
 
 import { ImpactChartProps } from './types';
 
-ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, ChartTooltip);
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, ChartTooltip, ArcElement);
+
+// const createBgGradient = (ctx: CanvasRenderingContext2D, categoryColor: string) => {
+//   console.log(ctx, categoryColor);
+//   if (ctx && categoryColor) {
+//     var x = 100,
+//       y = 75,
+//       // Radii of the white glow.
+//       innerRadius = 5,
+//       outerRadius = 70,
+//       // Radius of the entire circle.
+//       radius = 60;
+
+//     var gradient = ctx.createRadialGradient(212.5, 212.5, 0, 212.5, 212.5, 300.52);
+//     gradient.addColorStop(0, 'rgba(255, 255, 255, 1)');
+//     gradient.addColorStop(0.05, 'rgba(255, 255, 255, 1)');
+//     gradient.addColorStop(0.05, 'rgba(0, 188, 212, 0.14)');
+//     gradient.addColorStop(1, 'rgba(0, 182, 205, 1)');
+
+//     // ctx.arc(x, y, radius, 0, 2 * Math.PI);
+
+//     ctx.fillStyle = gradient;
+//     ctx.fill();
+//   }
+// };
+
+const hex2rgba = (hex: string, alpha: number) => {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
 
 export const ImpactChart: FC<ImpactChartProps> = ({
   className,
@@ -34,6 +67,10 @@ export const ImpactChart: FC<ImpactChartProps> = ({
 }) => {
   const { formatMessage } = useIntl();
   const chartDivRef = useRef();
+  const chartRef = useRef<ChartJS<'polarArea', number[], unknown>>(null);
+  const [chartDataSet, setChartDataSet] = useState<ChartData<'polarArea', number[]>['datasets']>(
+    []
+  );
 
   const {
     data: { impact: allImpacts, category: allCategories },
@@ -82,8 +119,29 @@ export const ImpactChart: FC<ImpactChartProps> = ({
     );
   }, [impactData, impactIds]);
 
-  const chartData: ChartData<'radar'> = useMemo(
-    () => ({
+  // useEffect(() => {
+  //   const chart = chartRef.current;
+
+  //   console.log(chart?.ctx, categoryColor);
+
+  //   if (chart && categoryColor) {
+  //     setChartDataSet([
+  //       {
+  //         data,
+  //         borderColor: 'white',
+  //         borderJoinStyle: 'round',
+  //         borderWidth: 2,
+  //         pointBackgroundColor: 'rgba(255, 159, 64, 0)',
+  //         pointBorderColor: 'rgba(255, 159, 64, 0)',
+  //         backgroundColor: createBgGradient(chart?.ctx, categoryColor),
+  //       },
+  //     ]);
+  //   }
+  // }, []);
+
+  const chartData: ChartData<'polarArea'> = useMemo(() => {
+    const chart = chartRef.current;
+    return {
       labels: [
         formatMessage({ defaultMessage: 'Biodiversity', id: 'mbTJWV' }),
         formatMessage({ defaultMessage: 'Climate', id: 'MuOp0t' }),
@@ -93,25 +151,44 @@ export const ImpactChart: FC<ImpactChartProps> = ({
       datasets: [
         {
           data,
-          backgroundColor: categoryColor,
-          borderColor: categoryColor,
+          borderColor: 'white',
           borderJoinStyle: 'round',
-          borderWidth: 1,
-          tension: 0.25,
+          offset: 10,
+          borderWidth: 0,
           pointBackgroundColor: 'rgba(255, 159, 64, 0)',
           pointBorderColor: 'rgba(255, 159, 64, 0)',
+          backgroundColor: () => {
+            const ctx = chart?.ctx;
+            if (ctx && categoryColor) {
+              var x = ctx.canvas.offsetWidth / 2,
+                y = ctx.canvas.offsetHeight / 2,
+                // Radii of the white glow.
+                innerRadius = 0,
+                // Radius of the entire circle.
+                radius = ctx.canvas.offsetHeight * 0.7;
+
+              var gradient = ctx.createRadialGradient(x, y, innerRadius, x, y, radius);
+              gradient.addColorStop(0, 'rgba(255, 255, 255, 1)');
+              gradient.addColorStop(0.05, 'rgba(255, 255, 255, 1)');
+              gradient.addColorStop(0.05, hex2rgba(categoryColor, 0.4));
+              gradient.addColorStop(1, categoryColor);
+
+              ctx.fillStyle = gradient;
+              ctx.fill();
+            }
+          },
         },
       ],
-    }),
-    [formatMessage, data, categoryColor]
-  );
+    };
+  }, [formatMessage, data, categoryColor]);
 
-  const chartOptions: ChartOptions<'radar'> = {
+  const chartOptions: ChartOptions<'polarArea'> = {
     scales: {
       r: {
         angleLines: {
-          color: 'rgba(227, 222, 214, 0.5)',
+          color: 'white',
           display: !isPlaceholder,
+          lineWidth: 4,
         },
         pointLabels: {
           display: false,
@@ -119,20 +196,19 @@ export const ImpactChart: FC<ImpactChartProps> = ({
         grid: {
           circular: true,
           color: 'rgba(227, 222, 214, 1)',
-          borderDash: (context: { index: number }) => {
-            return isPlaceholder || context.index === 0 ? [2, 2] : [];
-          },
+          borderDash: [2, 2],
         },
         beginAtZero: true,
         ticks: {
-          count: 3,
-          color: isPlaceholder ? 'rgba(88, 88, 88, 0.5)' : 'rgba(88, 88, 88, 1)',
-          font: { family: 'Work Sans', size: 14, weight: '600' },
+          count: compactMode ? 3 : 5,
+          color: 'rgba(153, 153, 153, 1)',
+          font: { family: 'Work Sans', size: 12, weight: '400' },
           backdropColor: 'transparent',
           display: !compactMode,
         },
         max: 10,
         min: 0,
+        // suggestedMin: 2,
       },
     },
     plugins: {
@@ -205,7 +281,7 @@ export const ImpactChart: FC<ImpactChartProps> = ({
                     reference={chartDivRef}
                   >
                     <div ref={chartDivRef}>
-                      <Radar data={chartData} options={chartOptions} />
+                      <PolarArea ref={chartRef} data={chartData} options={chartOptions} />
                     </div>
                   </Tooltip>
                 </Tooltip>
@@ -239,9 +315,7 @@ export const ImpactChart: FC<ImpactChartProps> = ({
               </span>
             )}
             <span className="relative flex items-center w-full overflow-x-hidden aspect-square">
-              <span className="absolute bg-white rounded-full opacity-50 top-2 right-2 bottom-2 left-2" />
-              <span className="absolute scale-50 bg-white rounded-full top-2 right-2 bottom-2 left-2 opacity-60" />
-              <Radar className="z-10" data={chartData} options={chartOptions} />
+              <PolarArea ref={chartRef} className="z-10" data={chartData} options={chartOptions} />
             </span>
             {allImpacts && (
               <span className="flex items-center justify-start ml-2">
