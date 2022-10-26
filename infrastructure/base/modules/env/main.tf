f7f4d1328a9287a7fc0b1365554b4e1f4c117b5c
Tiago Garcia <tiagojsag@gmail.com>
2022-10-26 15:38:47 +0200

    Improve HTTP basic auth env handling on TF; Document HTTP Basic auth env vars for the FE App

--- infrastructure/base/modules/env/main.tf
+++ infrastructure/base/modules/env/main.tf
@@ -80,6 +80,8 @@ module "sendgrid_api_key" {
 }
 
 module "http_auth_password" {
+  count            = length(var.http_auth_password) > 0 ? 1 : 0
+
   source           = "../secret_value"
   region           = var.gcp_region
   key              = "${var.project_name}_http_auth_password"
@@ -97,8 +99,6 @@ locals {
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY = var.google_maps_api_key
     NEXT_PUBLIC_MAPBOX_API_TOKEN    = var.mapbox_api_key
     NEXT_PUBLIC_HOTJAR_SITE_ID      = var.hotjar_site_id
-    NEXT_PUBLIC_HTTP_AUTH_USERNAME  = var.http_auth_username
-    NEXT_PUBLIC_HTTP_AUTH_PASSWORD  = var.http_auth_password
   }
   backend_docker_build_args = {
     RAILS_RELATIVE_URL_ROOT = "/backend"
@@ -164,6 +164,18 @@ module "frontend_cloudrun" {
   min_scale          = var.frontend_min_scale
   max_scale          = var.frontend_max_scale
   tag                = var.tag
+  secrets            = flatten([
+    length(module.http_auth_password) > 0 ? [{
+      name        = "HTTP_AUTH_PASSWORD"
+      secret_name = module.http_auth_password[0].secret_name
+    }] : []
+  ])
+  env_vars = [
+    {
+      name  = "HTTP_AUTH_USERNAME"
+      value = var.http_auth_username
+    }
+  ]
 }
 
 module "backend_cloudrun" {
@@ -179,7 +191,7 @@ module "backend_cloudrun" {
   min_scale          = var.backend_min_scale
   max_scale          = var.backend_max_scale
   tag                = var.tag
-  secrets            = [
+  secrets            = flatten([
     {
       name        = "SECRET_KEY_BASE"
       secret_name = module.rails_secret_key_base.secret_name
@@ -201,11 +213,11 @@ module "backend_cloudrun" {
     }, {
       name        = "ENCRYPTION_DERIVATION_SALT"
       secret_name = module.rails_encryption_derivation_salt.secret_name
-    }, {
+    }, length(module.http_auth_password) > 0 ? [{
       name        = "HTTP_AUTH_PASSWORD"
-      secret_name = module.http_auth_password.secret_name
-    }
-  ]
+      secret_name = module.http_auth_password[0].secret_name
+    }] : []
+  ])
   env_vars = [
     {
       name  = "DATABASE_NAME"
