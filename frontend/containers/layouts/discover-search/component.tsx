a422e4e0ad8ae749e08f9eebb06f78241061c9f3
Bárbara Chaves <barbarabarcellos90@gmail.com>
2022-10-28 16:01:16 +0200

    Update frontend/containers/layouts/discover-search/component.tsx
    Co-authored-by: Clément Prod'homme <clementprdhomme@users.noreply.github.com>

--- frontend/containers/layouts/discover-search/component.tsx
+++ frontend/containers/layouts/discover-search/component.tsx
@@ -159,7 +159,7 @@ export const DiscoverSearch: FC<DiscoverSearchProps> = ({ className }) => {
                     onClick={handleClickFilters}
                     aria-expanded={openFilters}
                     aria-controls="filters"
-                    className="!px-3 sm!py-3 sm:!px-6 !py-2"
+                    className="!px-3 !py-3 sm:!px-6 !py-2"
                   >
                     <span className="sm:hidden">
                       <Icon icon={FilterIcon} className="w-4 h-4 text-white" />
