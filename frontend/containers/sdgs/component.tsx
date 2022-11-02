878a38fbe8e5415276548a7183e9469290c04a6f
Clément Prod'homme <contact@clementprodhomme.fr>
2022-11-02 17:12:43 +0100

    Display the SDGs in 3 columns on mobile

--- frontend/containers/sdgs/component.tsx
+++ frontend/containers/sdgs/component.tsx
@@ -4,42 +4,48 @@ import cx from 'classnames';
 
 import Image from 'next/image';
 
+import { useBreakpoint } from 'hooks/use-breakpoint';
+
 import Tooltip from 'components/tooltip';
 
 import { SDGS_SIZES, SDGS_IMAGES } from './constants';
 import { SDGsProps } from './types';
 
-export const SDGs: FC<SDGsProps> = ({ className, size = 'small', sdgs = [] }: SDGsProps) => (
-  <div className={cx('flex flex-wrap items-center gap-2', className)}>
-    {sdgs.map((sdg) => (
-      <Fragment key={sdg.id}>
-        <Tooltip
-          arrow
-          arrowClassName="bg-black"
-          content={
-            <div className="max-w-xs p-2 font-sans text-sm font-normal text-white bg-black rounded-sm">
-              {sdg.name}
-            </div>
-          }
-        >
-          <div
-            // This removes an extra space displayed below the image when the container wraps
-            className="flex"
+export const SDGs: FC<SDGsProps> = ({ className, size = 'small', sdgs = [] }: SDGsProps) => {
+  const screenLargerThanSm = useBreakpoint()('sm');
+
+  return (
+    <div className={cx('grid grid-cols-3 sm:flex sm:flex-wrap sm:items-center gap-2', className)}>
+      {sdgs.map((sdg) => (
+        <Fragment key={sdg.id}>
+          <Tooltip
+            arrow
+            arrowClassName="bg-black"
+            content={
+              <div className="max-w-xs p-2 font-sans text-sm font-normal text-white bg-black rounded-sm">
+                {sdg.name}
+              </div>
+            }
           >
-            <Image
-              className="rounded"
-              src={SDGS_IMAGES[sdg.id]}
-              alt={sdg.name}
-              width={SDGS_SIZES[size]}
-              height={SDGS_SIZES[size]}
-              layout="intrinsic"
-              quality={100}
-            />
-          </div>
-        </Tooltip>
-      </Fragment>
-    ))}
-  </div>
-);
+            <div
+              // This removes an extra space displayed below the image when the container wraps
+              className="sm:flex"
+            >
+              <Image
+                className="rounded"
+                src={SDGS_IMAGES[sdg.id]}
+                alt={sdg.name}
+                width={SDGS_SIZES[size]}
+                height={SDGS_SIZES[size]}
+                layout={screenLargerThanSm ? 'intrinsic' : 'responsive'}
+                quality={100}
+              />
+            </div>
+          </Tooltip>
+        </Fragment>
+      ))}
+    </div>
+  );
+};
 
 export default SDGs;
