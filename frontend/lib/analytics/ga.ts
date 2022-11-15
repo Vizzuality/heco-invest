43b123e6e253af074ca818e898a6be3a02b2a3ab
Clément Prod'homme <contact@clementprodhomme.fr>
2022-11-15 10:52:20 +0100

    Update the GA helper

--- frontend/lib/analytics/ga.ts
+++ frontend/lib/analytics/ga.ts
@@ -1,13 +1,11 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
-
-// log the pageview with their URL
-export const GAPage = (url: string): void => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
-
-// log specific events happening.
-export const GAEvent = ({ action, params }): void => {
-  window.gtag('event', action, params);
+/**
+ * Log an event in Google Analytics
+ */
+export const logEvent = (
+  name: string,
+  params?: Record<string, string | number | boolean>
+): void => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', name, params);
+  }
 };
