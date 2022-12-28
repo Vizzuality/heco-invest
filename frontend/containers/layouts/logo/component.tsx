b999e24d39288fd7af7c59ed8355185d4a4abf58
barbara-chaves <barbarabarcellos90@gmail.com>
2022-12-28 14:12:55 +0100

    Change logo

--- frontend/containers/layouts/logo/component.tsx
+++ frontend/containers/layouts/logo/component.tsx
@@ -1,17 +1,53 @@
 import cx from 'classnames';
 
+import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { useScrollY } from 'hooks/use-scroll-y';
 
 import { Paths } from 'enums';
 
+const blackLogoPaths: string[] = [
+  Paths.ForProjectDevelopers,
+  Paths.ForInvestors,
+  Paths.About,
+  Paths.FAQ,
+  Paths.TermsConditions,
+];
+
 const Logo = () => {
   const { isScrolledY } = useScrollY();
+  const { pathname } = useRouter();
+  const showBlackLogo = blackLogoPaths.includes(pathname);
+
   return (
-    <Link href={Paths.Home}>
-      <a className="text-sm font-semibold xl:text-base">
-        HeCo Invest
+    <Link href={Paths.Home} passHref>
+      <a className="flex items-center text-sm font-semibold xl:text-base">
+        {isScrolledY ? (
+          <Image
+            width={150}
+            height={50}
+            objectFit="cover"
+            objectPosition="center"
+            src="/images/logos/logo-heco-invest-colors.png"
+            alt="Heco invest"
+            className=""
+          />
+        ) : (
+          <Image
+            width={150}
+            height={50}
+            objectFit="cover"
+            src={
+              showBlackLogo
+                ? '/images/logos/logo-heco-invest-black.png'
+                : '/images/logos/logo-heco-invest-white.png'
+            }
+            alt="Heco invest"
+            className="inline"
+          />
+        )}
         <span
           className={cx('text-sm ml-1 px-1 py-0.5 rounded-[4px]', {
             'bg-white text-green-dark': !isScrolledY,
