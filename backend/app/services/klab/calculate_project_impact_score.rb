ad89f6287ce8fae2bbd0397546a29b0c94e35a65
martintomas <martintomas.it@gmail.com>
2023-01-19 11:40:56 +0100

    feat: Finalisation of klab poll_impact_demands job

--- backend/app/services/klab/calculate_project_impact_score.rb
+++ backend/app/services/klab/calculate_project_impact_score.rb
@@ -2,17 +2,6 @@ module Klab
   class CalculateProjectImpactScore
     ImpactScoreStruct = Struct.new(:biodiversity, :climate, :community, :water)
 
-    INDICATORS = {
-      biodiversity: "im:Indicator (value of ecology:Biodiversity)",
-      climate: "im:Indicator (value of ecology:Ecosystem for es:ClimateRegulation)",
-      community: "im:Indicator (es.nca:Condition of demography:Human demography:Community)",
-      water: "im:Indicator (es.nca:Condition of earth:Aquatic ecology:Ecosystem)"
-    }.freeze
-
-    def self.observable_names
-      INDICATORS.values
-    end
-
     # @param geometry [String] geometry in format recognised by ARIES
     def initialize(geometry)
       @geometry = geometry
@@ -31,12 +20,12 @@ module Klab
 
     def get_result_from_api
       context_resp = Klab::SubmitContext.new(@client).call(@geometry)
-      ticket_resp = Klab::PollTicket.new(@client).call(context_resp.ticket_id)
+      ticket_resp = Klab::PollTicket.new(@client).call(context_resp.ticket_id, async: false)
       result = ImpactScoreStruct.new
       ticket_resp.artifacts_ids.each.with_index do |artifact_id, idx|
         Rails.logger.debug "Exporting observable #{artifact_id}"
         export_resp = ExportArtifact.new(@client).call(artifact_id)
-        artifact_code = INDICATORS.keys[idx]
+        artifact_code = Klab::SubmitContext::Request::INDICATORS.keys[idx]
         result.send("#{artifact_code}=", export_resp.summary["mean"]) if artifact_code.present?
       end
       result
