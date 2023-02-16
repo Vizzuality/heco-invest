2a8184fbc6b3800a53a3c44e8dabef11fb035211
martintomas <martintomas.it@gmail.com>
2023-02-16 10:44:46 +0100

    Update 20230203122726_recalculate_project_impacts.rb
    refactoring: Trigger recalculation only of KLAB_ENABLED is enabled

--- backend/db/migrate/20230203122726_recalculate_project_impacts.rb
+++ backend/db/migrate/20230203122726_recalculate_project_impacts.rb
@@ -1,5 +1,5 @@
 class RecalculateProjectImpacts < ActiveRecord::Migration[7.0]
   def change
-    Rake::Task["klab:recalculate_impacts"].execute
+    Rake::Task["klab:recalculate_impacts"].execute if ENV["KLAB_ENABLED"].to_s == "true"
   end
 end
