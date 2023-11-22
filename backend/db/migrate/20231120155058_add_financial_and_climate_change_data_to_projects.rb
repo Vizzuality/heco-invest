fac7d4c36b45fcf65c84e5eb7571f99ee544e5e1
yulia-bel <yuliya.beliakova@gmail.com>
2023-11-22 14:13:46 +0100

    Update last_year_sales_revenue attribute

--- backend/db/migrate/20231120155058_add_financial_and_climate_change_data_to_projects.rb
+++ backend/db/migrate/20231120155058_add_financial_and_climate_change_data_to_projects.rb
@@ -3,9 +3,7 @@ class AddFinancialAndClimateChangeDataToProjects < ActiveRecord::Migration[7.0]
     add_column :projects, :positive_financial_returns_en, :text, null: true
     add_column :projects, :positive_financial_returns_es, :text, null: true
     add_column :projects, :positive_financial_returns_pt, :text, null: true
-    add_column :projects, :last_year_sales_revenue_en, :text, null: true
-    add_column :projects, :last_year_sales_revenue_es, :text, null: true
-    add_column :projects, :last_year_sales_revenue_pt, :text, null: true
+    add_column :projects, :last_year_sales_revenue, :decimal, null: true
     add_column :projects, :climate_change_risks_details_en, :text, null: true
     add_column :projects, :climate_change_risks_details_es, :text, null: true
     add_column :projects, :climate_change_risks_details_pt, :text, null: true
